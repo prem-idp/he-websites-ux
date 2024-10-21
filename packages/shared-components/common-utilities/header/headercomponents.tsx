3b5c376d0b150b5235516bf80a9f19604af977a3
Udhaya kumar <udhayakumar.d@idp.com>
2024-10-21 11:47:19 +0530

    #HEW-314 #comments header added

--- packages/shared-components/common-utilities/header/headercomponents.tsx
+++ packages/shared-components/common-utilities/header/headercomponents.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React from "react";
+
+import React, { Fragment } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+
 import Megamenucomponents from "../topnav/megamenucomponents";
-import TopRightMenu from "./rightside-menu";
 
 const Header = () => {
   // Toggle Menu
@@ -14,15 +15,89 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 991);
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+// Check screen width
+useEffect(() => {
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 991);
+  };
 
+  // Initial check
+  handleResize();
+  // Add event listener on resize
+  window.addEventListener('resize', handleResize);
+
+  // Cleanup event listener on unmount
+  return () => window.removeEventListener('resize', handleResize);
+}, []);
+
+  // search click
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+  const [isUserClicked, setIsUserClicked] = useState(false);
+  const [isShortlistClicked, setIsShortlistClicked] = useState(false);
+
+  // right menu actions
+  const rightMenuAction = (actionName: string) => {
+    if (actionName == "SEARCH") {
+      setIsSearchClicked(!isSearchClicked);
+      setIsUserClicked(false);
+      setIsShortlistClicked(false);
+    } else if (actionName == "USER") {
+      setIsUserClicked(!isUserClicked);
+      setIsSearchClicked(false);
+      setIsShortlistClicked(false);
+    } else if (actionName == "SHORTLIST") {
+      setIsShortlistClicked(!isShortlistClicked);
+      setIsSearchClicked(false);
+      setIsUserClicked(false);
+    }
+  };
+
+  // course tab click
+  const [activeTab, setActiveTab] = useState("tab1");
+  const searchTabClick = (tabName: any) => {
+    setActiveTab(tabName);
+  };
+
+  // course tab actions
+  const [isUndergratuateClicked, setIsUndergratuateClicked] = useState(false);
+  const [isSubjectClicked, setIsSubjectClicked] = useState(false);
+  const [isLocationClicked, setIsLocationClicked] = useState(false);
+  const [isUniversityClicked, setIsUniversityClicked] = useState(false);
+  const [isAdviceClicked, setIsAdviceClicked] = useState(false);
+
+  const courseActions = (tabName: string) => {
+    if (tabName == "UG") {
+      setIsUndergratuateClicked(!isUndergratuateClicked);
+      setIsSubjectClicked(false);
+      setIsLocationClicked(false);
+      setIsUniversityClicked(false);
+      setIsAdviceClicked(false);
+    } else if (tabName == "Subject") {
+      setIsSubjectClicked(!isSubjectClicked);
+      setIsUndergratuateClicked(false);
+      setIsLocationClicked(false);
+      setIsUniversityClicked(false);
+      setIsAdviceClicked(false);
+    } else if (tabName == "Location") {
+      setIsLocationClicked(!isLocationClicked);
+      setIsUndergratuateClicked(false);
+      setIsSubjectClicked(false);
+      setIsUniversityClicked(false);
+      setIsAdviceClicked(false);
+    } else if (tabName == "University") {
+      setIsUniversityClicked(!isUniversityClicked);
+      setIsUndergratuateClicked(false);
+      setIsSubjectClicked(false);
+      setIsLocationClicked(false);
+      setIsAdviceClicked(false);
+    } else if (tabName == "Advice") {
+      setIsAdviceClicked(!isAdviceClicked);
+      setIsUndergratuateClicked(false);
+      setIsSubjectClicked(false);
+      setIsLocationClicked(false);
+      setIsUniversityClicked(false);
+    }
+  };
   return (
     <>
       <header className="bg-white pl-[16px] pr-[21px] py-[4px] xl:px-0 xl:py-[8px]">
@@ -41,11 +116,7 @@ const Header = () => {
           </div>
           <div className="order-1 md:grow md:basis-[100%] lg:order-2 lg:grow-1 lg:basis-0">
             {isMobile && (
-              <button
-                className="mr-[16px] block lg:hidden"
-                onClick={mobileToggleOpen}
-                aria-label="Mobile Toggle"
-              >
+            <button className="mr-[16px] block lg:hidden" onClick={mobileToggleOpen} aria-label="Mobile Toggle">
                 <svg
                   width="24"
                   height="24"
@@ -61,14 +132,14 @@ const Header = () => {
                     strokeLinejoin="round"
                   />
                 </svg>
-              </button>
+            </button>
             )}
             {isMobile ? (
               <>
               <div className={`${isOpen ? 'animate-fadeIn block' : 'hidden'} bg-neutral400 lg:bg-transparent fixed top-0 left-0 right-0 bottom-0 z-[5]`}></div>
                <div className={`megamenu-container fixed left-0 top-0 z-[6] w-full h-[100vh] lg:h-auto transition-all duration-300 ease-in-out ${isOpen ? 'animate-fadeInLeft' :'-translate-x-full'}`}>
                 
-              <>
+              <Fragment>
               { isOpen && (
                 <div className="relative z-[6] w-fit">
                     <div onClick={mobileToggleOpen} className={`menu-close-card lg:hidden absolute right-[-40px]`}>
@@ -82,7 +153,7 @@ const Header = () => {
                   <Megamenucomponents/>
                 </div>                         
                )}                         
-            </>
+            </Fragment>
             </div>
             </>
               ) : (
@@ -705,14 +776,132 @@ const Header = () => {
                         </div>
                       </div>
                     </div>
-                  )}
-                </>
-              </div>
-            ) : (
-              <Megamenucomponents />
-            )}
+                  </div>
+                )}
+              </li>
+              <li className="relative">
+                <Link
+                  onClick={() => rightMenuAction("USER")}
+                  href="#"
+                  aria-label="User"
+                  className="border border-gray-500 rounded-[34px] p-[14px] w-[48px] h-[48px] block"
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.3332 5.83333C13.3332 7.67428 11.8408 9.16667 9.99984 9.16667C8.15889 9.16667 6.6665 7.67428 6.6665 5.83333C6.6665 3.99238 8.15889 2.5 9.99984 2.5C11.8408 2.5 13.3332 3.99238 13.3332 5.83333Z"
+                      stroke="#5C656E"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M9.99984 11.6667C6.77818 11.6667 4.1665 14.2783 4.1665 17.5H15.8332C15.8332 14.2783 13.2215 11.6667 9.99984 11.6667Z"
+                      stroke="#5C656E"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Link>
+                {/* user section */}
+                {isUserClicked && (
+                  <div className="flex justify-between p-[16px] absolute top-[56px] right-[-39px] shadow-custom-5 bg-white min-w-[339px] rounded-[4px] lg:top-[71px] lg:right-0">
+                    <ul className="small">
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">My profile</Link>
+                      </li>
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">Favourites</Link>
+                      </li>
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">Profile item</Link>
+                      </li>
+                      <li className="text-primary-400 hover:underline">
+                        <Link href="#">Log out</Link>
+                      </li>
+                    </ul>
+                    <Link
+                      href="#"
+                      className="font-semibold x-small text-success-400 uppercase tracking-[1px]"
+                    >
+                      Complete your profile
+                    </Link>
+                  </div>
+                )}
+              </li>
+              <li className="relative min-h-[48px] flex items-center">
+                <Link
+                  onClick={() => rightMenuAction("SHORTLIST")}
+                  href="#"
+                  className="relative block"
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.31802 6.31802C2.56066 8.07538 2.56066 10.9246 4.31802 12.682L12.0001 20.364L19.682 12.682C21.4393 10.9246 21.4393 8.07538 19.682 6.31802C17.9246 4.56066 15.0754 4.56066 13.318 6.31802L12.0001 7.63609L10.682 6.31802C8.92462 4.56066 6.07538 4.56066 4.31802 6.31802Z"
+                      fill="#00BBFD"
+                      stroke="#3460DC"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <div className="absolute inline-flex items-center justify-center w-[16px] h-[16px] rounded-[8px] top-[10px] left-[13px] bg-success-400 text-black font-inter font-semibold text-xs-small px-[5px] py-[2px]">
+                    2
+                  </div>
+                </Link>
+
+                {/* shortlist section */}
+                {isShortlistClicked && (
+                  <div className="flex justify-between p-[16px] absolute top-[56px] right-[-5px] shadow-custom-5 bg-white min-w-[339px] rounded-[4px] lg:top-[71px]">
+                    <ul className="small">
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">
+                          Favourites
+                          <span className="w-[16px] h-[16px] rounded-[8px] bg-success-400 ml-[8px] text-black font-semibold xs-small px-[5px] py-[2px]">
+                            5
+                          </span>
+                        </Link>
+                      </li>
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">
+                          Courses
+                          <span className="w-[16px] h-[16px] rounded-[8px] bg-success-400 ml-[8px] text-black font-semibold xs-small px-[5px] py-[2px]">
+                            2
+                          </span>
+                        </Link>
+                      </li>
+                      <li className="mb-[16px] hover:underline">
+                        <Link href="#">
+                          Universities
+                          <span className="w-[16px] h-[16px] rounded-[8px] bg-success-400 ml-[8px] text-black font-semibold xs-small px-[5px] py-[2px]">
+                            3
+                          </span>
+                        </Link>
+                      </li>
+                    </ul>
+                    <Link
+                      href="#"
+                      className="font-semibold small bg-primary-400 text-white px-[16px] py-[8px] rounded-[18px] self-start"
+                    >
+                      Compare
+                    </Link>
+                  </div>
+                )}
+              </li>
+            </ul>
           </div>
-          <TopRightMenu />
         </div>
       </header>
     </>
