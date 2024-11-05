3a3c97df318a34573e98d70ad8b640f8cbcd08e1
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-05 14:50:09 +0530

    #ux-sprint-6 #comments updated

--- apps/pgs/src/app/page.tsx
+++ apps/pgs/src/app/page.tsx
@@ -2,16 +2,16 @@
 import React from "react";
 import Herocomponents from "@packages/shared-components/home/hero/herocomponents";
 import AdviceComponent from "@packages/shared-components/home/advice/advicecomponents";
-import DiscoverComponent from "@packages/shared-components/home/discover/discovercomponents";
+import Discovercomponents from "@packages/shared-components/home/discover/discovercomponents";
 import ReviewComponent from "@packages/shared-components/home/reviews/reviewscomponents";
 import TestimonialComponent from "@packages/shared-components/home/testimonials/testimonialcomponents";
-import WucasComponent from "@packages/shared-components/home/wuscas/wuscascomponents";
+import Wuscascomponents from "@packages/shared-components/home/wuscas/wuscascomponents";
 const page = () => {
   return (
     <>
       <Herocomponents project="pgs" />
-      <WucasComponent />
-      <DiscoverComponent />
+      <Wuscascomponents />
+      <Discovercomponents />
       <AdviceComponent />
       <TestimonialComponent />
       <ReviewComponent />
--- packages/shared-components/home/wuscas/wuscascomponents.tsx
+++ packages/shared-components/home/wuscas/wuscascomponents.tsx
@@ -1,3 +1,4 @@
+"use server";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
