c70e4aec684403563321c7f57ae5e01055135b5e
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-08 16:18:24 +0530

    #footer #comments dynamic import added

--- apps/whatuni/next.config.mjs
+++ apps/whatuni/next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(woff|woff2)$/,
+  //     use: {
+  //       loader: "file-loader",
+  //       options: {
+  //         outputPath: "static/fonts",
+  //         publicPath: "/_next/static/fonts",
+  //         name: "[name].[ext]",
+  //       },
+  //     },
+  //   });
+  //   return config;
+  // },
   images: {
     remotePatterns: [
       {
