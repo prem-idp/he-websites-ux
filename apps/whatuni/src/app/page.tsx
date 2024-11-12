0966586025b42568a302103fcb0f274da8a5ef03
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-12 10:36:57 +0530

    #HEW-507 #comments input mapping data added

--- apps/whatuni/src/app/page.tsx
+++ apps/whatuni/src/app/page.tsx
@@ -1,6 +1,39 @@
 "use server";
 import dynamic from "next/dynamic";
+// import { graphQlFetchFunction } from "@packages/lib/server-actions/server-action";
+// import { homePageQuery } from "@packages/lib/graphQL/graphql-query";
+import DynamicSkeleton from "@packages/lib/dynamic-imports/skeletons";
 const Page = async () => {
+  // const jsonData = await graphQlFetchFunction(homePageQuery);
+  // console.log(jsonData);
+  const data = {
+    __typename: "DynamicMediaComponent",
+    longDescription: null,
+    internalName: "Discover - Courses - Whatuni",
+    title: "Courses",
+    subTitle: "Looking for courses?",
+    shortDescription: null,
+    cta: {
+      internalName: "Homepage - Courses Discover - Whatuni",
+      primaryCtaUrl: "/degrees/courses/",
+      secondaryCtaUrl: null,
+      primaryCtaLabel: null,
+      secondaryCtaLabel: null,
+      primaryCtaTarget: null,
+      secondaryCtaTarget: null,
+      flagStyle: null,
+    },
+    image: {
+      imageTitle: null,
+      imgIntName: "Homepage - Courses Discover - Whatuni",
+      imgAltText: "courses",
+      imgUpload: {
+        url: "https://images.ctfassets.net/szez98lehkfm/4NX4x0aesX7abBMiTsUCDY/0b953f5873dfc1d36af1c6334827b240/discover-feature-image1x3x.webp",
+        width: 256,
+        height: 275,
+      },
+    },
+  };
   const componentList = [
     {
       component: "HeroSliderComponent",
@@ -62,7 +95,7 @@ const Page = async () => {
   ) =>
     dynamic(() => import(`@packages/shared-components/${path}`), {
       ssr: ssrValue,
-      loading: () => <p>{loadingComponent}</p>,
+      loading: () => <DynamicSkeleton skeletonName={loadingComponent} />,
     });
 
   return (
@@ -70,7 +103,7 @@ const Page = async () => {
       <div>
         {componentList.map(({ path, ssrValue, loadingComponent }, index) => {
           const Component = loadComponent(path, ssrValue, loadingComponent);
-          return <Component key={index} />;
+          return <Component key={index} data={data} />;
         })}
       </div>
     </>
