746f51961f934ef0ecec64c0c3ec1243c9e69d06
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-14 15:02:35 +0530

    #develop #comments whatuni stcak added with HE

--- apps/whatuni/cdk/lib/cdk-stack.ts
+++ apps/whatuni/cdk/lib/cdk-stack.ts
@@ -24,7 +24,7 @@ import {
 } from "aws-cdk-lib/aws-iam";
 import { MyLogGroupArm } from "./logGroupArn";
 
-export class WhatuniCdkStack extends cdk.Stack {
+export class WhatuniHeCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     const [VPC_SUBNET1_VALUE, VPC_SUBNET2_VALUE, VPC_SUBNET3_VALUE] =
       process.env.AWS_VPC_SUBNETS?.split(",") ?? [];
