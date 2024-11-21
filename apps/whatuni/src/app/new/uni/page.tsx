19c3494dc974d17f588299a13f26d167ac1f7e3f
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-21 16:31:01 +0530

    #develop #comments updated

--- apps/whatuni/src/app/new/uni/page.tsx
+++ apps/whatuni/src/app/new/uni/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
         <Link href="/new/article">Move to static page</Link>
       </h1>
       <h1>
-        <Link href="/new/article">Move to dynamic page</Link>
+        <Link href="/new/whatuni">Move to dynamic page</Link>
       </h1>
     </center>
   );
