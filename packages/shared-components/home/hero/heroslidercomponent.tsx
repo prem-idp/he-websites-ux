8f62280f7f39496ade951f909287f95e6776490a
Udhaya kumar <udhayakumar.d@idp.com>
2024-11-21 12:39:31 +0530

    #HEW-507 #comments skeleton issue fixed

--- packages/shared-components/home/hero/heroslidercomponent.tsx
+++ packages/shared-components/home/hero/heroslidercomponent.tsx
@@ -3,13 +3,13 @@ import React, { Suspense } from "react";
 import SearchWrapper from "./search-pod/searchwrapper";
 import HeroSlider from "./slider-pod/heroSlider";
 import { SliderBannerCollection } from "@packages/lib/types/interfaces";
-import HeroTempSkeleton from "@packages/shared-components/common-utilities/skeleton/temp";
+import HeroSliderComponentSkeleton from "@packages/shared-components/common-utilities/skeleton/heroslidercomponentskeleton";
 interface PropjectProps {
   data: SliderBannerCollection;
 }
 const HeroSliderComponent: React.FC<PropjectProps> = ({ data }) => {
   return (
-    <Suspense fallback={<HeroTempSkeleton />}>
+    <Suspense fallback={<HeroSliderComponentSkeleton />}>
       <div
         data-testid="hero-banner-colour"
         className={`${process.env.PROJECT === "Whatuni" ? "bg-blue-200" : "bg-yellow-200"} px-[16px] md:px-[20px] xl2:px-0.5`}
