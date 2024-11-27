d0fab0cae740e006c4f2c175a3d053192ee07af9
Manirathinam K <manirathinam.k@idp.com>
2024-11-27 22:26:23 +0530

    #feature/Hew-508#comments-google one tap fixed

--- packages/lib/utlils/GoogleOneTap.tsx
+++ packages/lib/utlils/GoogleOneTap.tsx
@@ -3,32 +3,28 @@ import { useEffect } from "react";
 
 const GoogleOneTap = () => {
   useEffect(() => {
-    if (typeof window !== "undefined" && (window as any).google) {
-      console.log("Google One Tap is ready");
-      (window as any).google.accounts.id.initialize({
-        client_id:
-          "310464352984-52q8deiepmmnslhkehui0llrmvlvq5lu.apps.googleusercontent.com", // Replace with your Google OAuth Client ID
-        callback: (response: any) => {
-          const { credential } = response;
-          console.log(response, "response");
-        },
-      });
-      (window as any).google.accounts.id.prompt(); // Automatically prompt Google One Tap
-    } else {
-      console.log("insndie the else");
-      setTimeout(() => {
+    const initializeGoogleOneTap = () => {
+      if (typeof window !== "undefined" && (window as any).google) {
+        console.log("Google One Tap is ready");
         (window as any).google.accounts.id.initialize({
           client_id:
             "310464352984-52q8deiepmmnslhkehui0llrmvlvq5lu.apps.googleusercontent.com", // Replace with your Google OAuth Client ID
           callback: (response: any) => {
             const { credential } = response;
             console.log(response, "response");
-            onLoginSuccess(response); // Handle the successful login
+            // Handle the successful login
           },
         });
         (window as any).google.accounts.id.prompt(); // Automatically prompt Google One Tap
-      }, 1000);
-    }
+      } else {
+        console.log("Google One Tap library not loaded yet. Retrying...");
+        setTimeout(() => {
+          initializeGoogleOneTap();
+        }, 1000);
+      }
+    };
+
+    initializeGoogleOneTap();
   }, []);
 
   // Return null if Google One Tap is not ready or not available
