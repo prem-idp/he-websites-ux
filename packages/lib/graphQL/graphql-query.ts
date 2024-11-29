2adb28abdef1d29369b945b019b4a0afd6b06ab6
Sujitha Velayutham <sujitha.v@idp.com>
2024-11-29 16:58:27 +0530

    HEW-501 #Commit for removing the limit for tag cloud pod query.
    Commit for removing the limit for tag cloud pod query.

--- packages/lib/graphQL/graphql-query.ts
+++ packages/lib/graphQL/graphql-query.ts
@@ -205,7 +205,7 @@ export const tagCloudQuery = `{
         items {
           __typename
           ... on MultipleCardContainer {
-            mediaCardsCollection(limit: 5) {
+            mediaCardsCollection {
               items {
                 __typename
                 ... on PageTagCloud {
