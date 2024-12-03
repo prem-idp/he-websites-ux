8804e49b47ece7d790fa5a4f76033335f6e3c110
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-03 11:11:39 +0530

    #develop #comments hash value added in both client and custom

--- apps/whatuni/src/app/api/graphql/route.ts
+++ apps/whatuni/src/app/api/graphql/route.ts
@@ -1,15 +1,20 @@
 import { NextResponse } from "next/server";
-// import crypto from "crypto";
-
+import crypto from "crypto";
 export async function POST(request: any) {
   try {
     const { query }: any = await request.json();
     console.log(query);
+    const payloadString = JSON.stringify(query);
+    const hash = crypto
+      .createHash("sha256")
+      .update(payloadString)
+      .digest("hex");
     const res = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHQL_AUTH}`,
+        "x-amz-content-sha256": hash,
       },
       body: JSON.stringify({ query: query }),
       next: { revalidate: 90 },
