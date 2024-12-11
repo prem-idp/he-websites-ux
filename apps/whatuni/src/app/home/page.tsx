d47e14fde9991a4c7b1c4a78109a4c49fc4db65a
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-11 15:04:36 +0530

    #HEW-373 #comments Routing changed

---
+++ apps/whatuni/src/app/home/page.tsx
@@ -0,0 +1,51 @@
+"use server";
+import dynamicComponentImports from "@packages/lib/dynamic-imports/imports";
+import Heroslidercomponent from "@packages/shared-components/home/hero/heroslidercomponent";
+import { graphQlFetchFunction } from "@packages/lib/server-actions/server-action";
+import { homePageQuery } from "@packages/lib/graphQL/graphql-query";
+import {
+  MultipleCardContainer,
+  SliderBannerCollection,
+} from "@packages/lib/types/interfaces";
+import GoogleOneTap from "@packages/lib/utlils/GoogleOneTap";
+import { headers } from "next/headers";
+import { fetchAuthSession } from "@aws-amplify/auth";
+import { Amplify } from "aws-amplify";
+import awsconfig from "../../../configs/amplifyconfiguration";
+Amplify.configure(awsconfig, { ssr: true });
+const Page = async () => {
+  const headersList = await headers(); // Await the promise
+  const isAuthenticated = headersList.get("isAuthenticated") || "false";
+  const jsonData = await graphQlFetchFunction(homePageQuery);
+  const componentList =
+    jsonData?.data?.contentData?.items[0]?.bodyContentCollection?.items;
+  const heroSliderData: SliderBannerCollection =
+    jsonData?.data?.contentData?.items[0]?.sliderBannerCollection;
+
+  return (
+    <>
+      <GoogleOneTap />
+      <Heroslidercomponent data={heroSliderData} />
+      <div>
+        {componentList.map(
+          (childItems: MultipleCardContainer, index: number) => {
+            const Component: any = dynamicComponentImports(
+              childItems.flagComponentStyle
+            );
+            return (
+              <Component
+                key={index}
+                heading={childItems?.cardSectionTitle}
+                subheading={childItems?.shortDescription}
+                internalName={childItems?.internalName}
+                callAction={childItems?.callToAction}
+              />
+            );
+          }
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
