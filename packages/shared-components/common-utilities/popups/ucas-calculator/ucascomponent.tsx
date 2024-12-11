d126e44be40b168353ef3aa4bca6b296a17f4145
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-11 11:51:15 +0530

    #HEW-373 #comments spinner added

--- packages/shared-components/common-utilities/popups/ucas-calculator/ucascomponent.tsx
+++ packages/shared-components/common-utilities/popups/ucas-calculator/ucascomponent.tsx
@@ -576,9 +576,31 @@ const UcasComponent = ({ onClose, isUcasOpen }: PropsInterface) => {
               Reset
             </Link>
             <button
-              className={`rounded-[24px] py-[10px] px-[16px] min-w-[200px] font-semibold ${(qual[0].SelectedLevel === "UCAS Tariff Points" && qual[0].min > qual[0].max) || JSON.stringify(qual) === JSON.stringify(qualCopy) ? "cursor-not-allowed bg-grey-100 text-grey-500" : "bg-primary-400 text-white hover:bg-primary-500"}`}
+              className={`inline-flex items-center rounded-[24px] py-[10px] px-[16px] min-w-[200px] font-semibold ${(qual[0].SelectedLevel === "UCAS Tariff Points" && qual[0].min > qual[0].max) || JSON.stringify(qual) === JSON.stringify(qualCopy) ? "cursor-not-allowed bg-grey-300 text-grey-500" : "bg-primary-400 text-white hover:bg-primary-500"}`}
               onClick={updateUcas}
             >
+              {applybtn == "Applying..." && (
+                <svg
+                  className="animate-spin mr-[8px] h-[18px] w-[18px] text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
               {applybtn}
             </button>
           </div>
