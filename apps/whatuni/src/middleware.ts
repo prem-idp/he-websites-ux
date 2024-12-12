6affe8a258a314f20d094c9416af895db03da67f
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-12 16:28:17 +0530

    Update middleware.ts

--- apps/whatuni/src/middleware.ts
+++ apps/whatuni/src/middleware.ts
@@ -3,46 +3,57 @@ import { NextRequest, NextResponse } from "next/server";
 import { runWithAmplifyServerContext } from "@packages/lib/utlils/amplifyServerUtils";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  const result = await runWithAmplifyServerContext({
-    nextServerContext: { request, response },
-    operation: async (contextSpec) => {
-      try {
-        const session = await fetchAuthSession(contextSpec);
-        // console.log(session.isValid());
-        // Check if tokens are not undefined
-        if (session.tokens) {
-          const hasAccessToken = session.tokens.accessToken !== undefined;
-          const hasIdToken = session.tokens.idToken !== undefined;
-
-          if (hasAccessToken && hasIdToken) {
-            return { authenticated: true, idToken: session.tokens.idToken };
-          }
-        }
-
-        return { authenticated: false, idToken: null };
-      } catch (error) {
-        return { authenticated: false, idToken: null };
-      }
+  // const response = NextResponse.next();
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("cookie", "text1");
+  const existingCookies = request.cookies.getAll();
+  const filteredCookies = existingCookies
+    .filter(
+      (cookie) =>
+        !["CognitoIdentityServiceProvider", "test"].includes(cookie.name)
+    )
+    .map((cookie) => `${cookie.name}=${cookie.value}`);
+  const finalCookieString = filteredCookies.join("; ");
+  requestHeaders.set("cookie", finalCookieString);
+  NextResponse.next({
+    request: {
+      headers: requestHeaders,
     },
   });
 
-  if (result.authenticated) {
-    // Set headers for authenticated users
-    response.headers.set("isAuthenticated", "true");
+  // const result = await runWithAmplifyServerContext({
+  //   nextServerContext: { request, response },
+  //   operation: async (contextSpec) => {
+  //     try {
+  //       const session = await fetchAuthSession(contextSpec);
+  //       if (session.tokens) {
+  //         const hasAccessToken = session.tokens.accessToken !== undefined;
+  //         const hasIdToken = session.tokens.idToken !== undefined;
 
-    // Ensure idToken is a string before setting it in headers
-    response.headers.set(
-      "idToken",
-      result.idToken ? String(result.idToken) : ""
-    );
-  } else {
-    // Set headers for unauthenticated users
-    response.headers.set("isAuthenticated", "false");
-  }
+  //         if (hasAccessToken && hasIdToken) {
+  //           return { authenticated: true, idToken: session.tokens.idToken };
+  //         }
+  //       }
 
-  return response;
+  //       return { authenticated: false, idToken: null };
+  //     } catch (error) {
+  //       return { authenticated: false, idToken: null };
+  //     }
+  //   },
+  // });
+  // if (result.authenticated) {
+  //   // Set headers for authenticated users
+  //   response.headers.set("isAuthenticated", "true");
+
+  //   // Ensure idToken is a string before setting it in headers
+  //   response.headers.set(
+  //     "idToken",
+  //     result.idToken ? String(result.idToken) : ""
+  //   );
+  // } else {
+  //   response.headers.set("isAuthenticated", "false");
+  // }
+  // return response;
 }
 
 export const config = {
