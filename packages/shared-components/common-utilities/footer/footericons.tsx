442e899b7b7cc5a8f475eb9ec0de71f6e5a452eb
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-17 14:25:05 +0530

    #HEW-373 #comments footer image icon breaking issue fixed

--- packages/shared-components/common-utilities/footer/footericons.tsx
+++ packages/shared-components/common-utilities/footer/footericons.tsx
@@ -11,14 +11,20 @@ const FooterIcons = ({ data }: PropsInterface) => {
     <ul className="flex flex-row gap-[16px]">
       {data?.map((item, index) => (
         <li key={index}>
-          <Link prefetch={false} href={item?.navUrl || ""} aria-label="facebook">
-            <Image
-              alt={item.navName}
-              src={item.navIcon.url}
-              width={item.navIcon.width}
-              height={item.navIcon.height}
-            />
-          </Link>
+          {item?.navIcon?.url && (
+            <Link
+              prefetch={false}
+              href={item?.navUrl || ""}
+              aria-label="facebook"
+            >
+              <Image
+                alt={item?.navName}
+                src={item?.navIcon.url}
+                width={item?.navIcon.width}
+                height={item?.navIcon.height}
+              />
+            </Link>
+          )}
         </li>
       ))}
     </ul>
