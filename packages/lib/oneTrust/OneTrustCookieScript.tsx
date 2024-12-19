a0ba1815275cb32fb4f44bf2f66605160ef33df7
Manirathinam K <manirathinam.k@idp.com>
2024-12-19 11:39:33 +0530

    #feature/Hew-508#comments-modify the cookie popup on load

--- packages/lib/oneTrust/OneTrustCookieScript.tsx
+++ packages/lib/oneTrust/OneTrustCookieScript.tsx
@@ -4,9 +4,9 @@ import Script from "next/script";
 import { useEffect, useState } from "react";
 import { createCookieConsent, getOnetrustCookieValue } from "./OneTrustcookie";
 
-  var OptanonConsent:string|undefined = undefined;
-  var OptanonAlertBoxClosed:string|undefined = undefined;
-  
+var OptanonConsent: string | undefined = undefined;
+var OptanonAlertBoxClosed: string | undefined = undefined;
+
 export default function OneTrustCookieScript() {
   const [useinteraction, setUserinteraction] = useState(false);
   const loadAnalyticsScripts = async (): Promise<boolean> => {
@@ -16,76 +16,114 @@ export default function OneTrustCookieScript() {
     );
 
     //if (window.Optanon && typeof window.Optanon.IsConsented === 'function') {
-  
-      const cookieDate = new Date();
-      //
-      const defaultCookieCategoryId = 'C0001'; 
-      const functionalCookieCategoryId = 'C0002';
-      const performanceCookieCategoryId = 'C0003';
-      const targetingCookieCategoryId = 'C0004';
-      //
-       OptanonConsent = await getOnetrustCookieValue('OptanonConsent');
-       OptanonAlertBoxClosed = await getOnetrustCookieValue('OptanonAlertBoxClosed');
-  
-      //
-      const strickCK = OnetrustActiveGroups.includes(defaultCookieCategoryId) ? "0" : "1"; 
-      const funCK = OnetrustActiveGroups.includes(functionalCookieCategoryId) ? "0" : "1"; 
-      const perCK = OnetrustActiveGroups.includes(performanceCookieCategoryId) ? "0" : "1"; 
-      const targetCK = OnetrustActiveGroups.includes(targetingCookieCategoryId) ? "0" : "1"; 
-  
-      //
-      const oneTrustCookieconsentVal = strickCK + funCK + perCK + targetCK;
-    
-      const isUserAcctpedCookie: boolean = (OptanonConsent && OptanonConsent != '') && (OptanonAlertBoxClosed && OptanonAlertBoxClosed != '') ? true : false;
-      const cookieConsentVal = isUserAcctpedCookie ? oneTrustCookieconsentVal : "0111";
-      console.log("IsAlertBoxClosed: ", isUserAcctpedCookie);
-      console.log("cookieConsentVal: ", cookieConsentVal);
-      console.log("OptanonConsent: ", OptanonConsent, "OptanonAlertBoxClosed: ", OptanonAlertBoxClosed);
-      
-      // --> dataLayerFn("cookieconsent_ga4", "NA", dataLabel, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA","NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA");
-      if(isUserAcctpedCookie){
-        const formattedDate = cookieDate.getDate() + '-' + cookieDate.getMonth() + '-' + cookieDate.getFullYear() + ' ' + cookieDate.getHours() + ':' + cookieDate.getMinutes() + ':' + cookieDate.getSeconds();
-        createCookieConsent('cookie_splash_flag_new',encodeURI(formattedDate));
-        createCookieConsent('cookie_splash_flag',encodeURI(formattedDate));
-      }
-      createCookieConsent('cookieconsent',cookieConsentVal);
-  
-      
-      // if (bannerCheck == "bannerExist") {
-      // 	var limitedAdStatus = targetCK == "0" ? true : false;
-      // 	window?.googletag?.cmd?.push(function() {
-      // 		window?.googletag?.pubads().setPrivacySettings({
-      // 			limitedAds: limitedAdStatus,
-      // 		});
-      // 	});
-      // 	displayBanner();
-      // }
-  
-      //loadDynamicJs("https://accounts.google.com/gsi/client"); */
-  //}
+
+    const cookieDate = new Date();
+    //
+    const defaultCookieCategoryId = "C0001";
+    const functionalCookieCategoryId = "C0002";
+    const performanceCookieCategoryId = "C0003";
+    const targetingCookieCategoryId = "C0004";
+    //
+    OptanonConsent = await getOnetrustCookieValue("OptanonConsent");
+    OptanonAlertBoxClosed = await getOnetrustCookieValue(
+      "OptanonAlertBoxClosed"
+    );
+
+    //
+    const strickCK = OnetrustActiveGroups.includes(defaultCookieCategoryId)
+      ? "0"
+      : "1";
+    const funCK = OnetrustActiveGroups.includes(functionalCookieCategoryId)
+      ? "0"
+      : "1";
+    const perCK = OnetrustActiveGroups.includes(performanceCookieCategoryId)
+      ? "0"
+      : "1";
+    const targetCK = OnetrustActiveGroups.includes(targetingCookieCategoryId)
+      ? "0"
+      : "1";
+
+    //
+    const oneTrustCookieconsentVal = strickCK + funCK + perCK + targetCK;
+
+    const isUserAcctpedCookie: boolean =
+      OptanonConsent &&
+      OptanonConsent != "" &&
+      OptanonAlertBoxClosed &&
+      OptanonAlertBoxClosed != ""
+        ? true
+        : false;
+    const cookieConsentVal = isUserAcctpedCookie
+      ? oneTrustCookieconsentVal
+      : "0111";
+    console.log("IsAlertBoxClosed: ", isUserAcctpedCookie);
+    console.log("cookieConsentVal: ", cookieConsentVal);
+    console.log(
+      "OptanonConsent: ",
+      OptanonConsent,
+      "OptanonAlertBoxClosed: ",
+      OptanonAlertBoxClosed
+    );
+
+    // --> dataLayerFn("cookieconsent_ga4", "NA", dataLabel, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA","NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA");
+    if (isUserAcctpedCookie) {
+      const formattedDate =
+        cookieDate.getDate() +
+        "-" +
+        cookieDate.getMonth() +
+        "-" +
+        cookieDate.getFullYear() +
+        " " +
+        cookieDate.getHours() +
+        ":" +
+        cookieDate.getMinutes() +
+        ":" +
+        cookieDate.getSeconds();
+      createCookieConsent("cookie_splash_flag_new", encodeURI(formattedDate));
+      createCookieConsent("cookie_splash_flag", encodeURI(formattedDate));
+    }
+    createCookieConsent("cookieconsent", cookieConsentVal);
+
+    // if (bannerCheck == "bannerExist") {
+    // 	var limitedAdStatus = targetCK == "0" ? true : false;
+    // 	window?.googletag?.cmd?.push(function() {
+    // 		window?.googletag?.pubads().setPrivacySettings({
+    // 			limitedAds: limitedAdStatus,
+    // 		});
+    // 	});
+    // 	displayBanner();
+    // }
+
+    //loadDynamicJs("https://accounts.google.com/gsi/client"); */
+    //}
     return isUserAcctpedCookie;
   };
 
   const watchOnetrustClosedcookie = async () => {
-    
-    var timeOutTime = setTimeout(async () => {
-      OptanonAlertBoxClosed = await getOnetrustCookieValue('OptanonAlertBoxClosed'); 
-      OptanonConsent = await getOnetrustCookieValue('OptanonConsent'); 
-      console.log("set time out OptanonAlertBoxClosed: " + OptanonAlertBoxClosed);
-      if((OptanonConsent && OptanonConsent != '') && (OptanonAlertBoxClosed && OptanonAlertBoxClosed != '')){
+     setTimeout(async () => {
+      OptanonAlertBoxClosed = await getOnetrustCookieValue(
+        "OptanonAlertBoxClosed"
+      );
+      OptanonConsent = await getOnetrustCookieValue("OptanonConsent");
+      console.log(
+        "set time out OptanonAlertBoxClosed: " + OptanonAlertBoxClosed
+      );
+      if (
+        OptanonConsent &&
+        OptanonConsent != "" &&
+        OptanonAlertBoxClosed &&
+        OptanonAlertBoxClosed != ""
+      ) {
         loadAnalyticsScripts();
-      } else{
-        watchOnetrustClosedcookie();
-      }
+      } 
     }, 1000);
-  }
+  };
 
-
-	const [userConsentGiven, setUserConsentGiven] = useState<boolean>(false);
+  const [userConsentGiven, setUserConsentGiven] = useState<boolean>(false);
   useEffect(() => {
     // Function to check consent, when event fires
     const handleConsentChange = async () => {
-	    console.log("OptanonWrapper function triggered...");
+      console.log("OptanonWrapper function triggered...");
       const returnVal = await loadAnalyticsScripts();
       setUserConsentGiven(() => returnVal);
     };
@@ -98,18 +136,17 @@ export default function OneTrustCookieScript() {
       window.OptanonWrapper = handleConsentChange;
       // Remove event listeners after loading the script
 
-      window.removeEventListener("mousemove", handleUserInteraction);
+      window.removeEventListener("load", handleUserInteraction);
     };
 
     // Add event listeners for user interaction
 
-    window.addEventListener("mousemove", handleUserInteraction);
+    window.addEventListener("load", handleUserInteraction);
 
     return () => {
-      window.removeEventListener("mousemove", handleUserInteraction);
+      window.removeEventListener("load", handleUserInteraction);
     };
   }, []);
-  
 
   return (
     <>
