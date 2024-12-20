e1a4088a6e09c7dde718a40230a88893efd14a45
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-20 17:34:30 +0530

    #HEW-373 #comments GA and GTM changes

--- apps/whatuni/src/app/home/layout.tsx
+++ apps/whatuni/src/app/home/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { graphQlFetchFunction } from "@packages/lib/server-actions/server-action";
 import { Seoquery } from "@packages/lib/graphQL/graphql-query";
 import TrackSessionId from "@packages/lib/utlils/tracksessionid";
-import GTMScript from "@packages/lib/utlils/gtmscript";
+//import GTMScript from "@packages/lib/utlils/gtmscript";
 import Script from "next/script";
 import dynamic from "next/dynamic";
 import { GoogleTagManager } from "@next/third-parties/google";
