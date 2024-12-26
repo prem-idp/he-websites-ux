50aa5a28a8b601a419fb01e11bc8c3a2bd0f673b
Udhaya kumar <udhayakumar.d@idp.com>
2024-12-26 13:20:28 +0530

    #HEW-373 #comments bug fixed

--- apps/pgs/src/app/home/layout.tsx
+++ apps/pgs/src/app/home/layout.tsx
@@ -1,11 +1,3 @@
-import type { Metadata } from "next";
-import { graphQlFetchFunction } from "@packages/lib/server-actions/server-action";
-import { Seoquery } from "@packages/lib/graphQL/graphql-query";
-//import { GoogleTagManager } from "@next/third-parties/google";
-//import GTMScript from "@packages/lib/utlils/gtmscript";
-import Script from "next/script";
-//import LoadGTMAfteruserInteraction from "@packages/lib/utlils/loadgtmafteruserinteraction";
-
 export default async function Layout({
   children,
 }: {
