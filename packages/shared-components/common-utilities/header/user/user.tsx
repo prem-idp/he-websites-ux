1eb76f5bde077871caa0284be517052c1db9cda5
Manirathinam K <manirathinam.k@idp.com>
2025-01-09 11:23:30 +0530

    #feature/Hew-508#comments-added the logout for pgs

--- packages/shared-components/common-utilities/header/user/user.tsx
+++ packages/shared-components/common-utilities/header/user/user.tsx
@@ -20,6 +20,7 @@ export default function User({ topnav_data }: any) {
     } else {
       document.cookie = `pgs_auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
       document.cookie = `pgs_bskt_cnt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
+      document.cookie = `pgs_x=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
 
       setTimeout(() => {
         const cookies: any = document.cookie
