c9ad1c5b007f97320dce01cd1cc9e974262afd15
Manirathinam K <manirathinam.k@idp.com>
2025-01-09 15:10:48 +0530

    #feature/HEW-508#comments-clean up the onetap

--- packages/lib/utlils/GoogleOneTapPgs.tsx
+++ packages/lib/utlils/GoogleOneTapPgs.tsx
@@ -10,26 +10,21 @@ const GoogleOneTapPgs = () => {
     const cookie = cookieArray.find((c) => c.startsWith(`${name}=`));
     return cookie ? cookie.split("=")[1] : "";
   }
+
   function extractdetailsforregister(text: any) {
     const parts = text.toString().split("##SPLIT##");
-    console.log(parts);
     if (parts) {
       const initial =
         parts[1] && parts[1] !== '""' ? parts[1].replace(/"/g, "") : " ";
       const count = parts[2] || " ";
       const sessionId = parts[3] || " ";
       if (initial) {
-        console.log(initial, "initila from the register");
         setCookie("pgs_auth", initial, 7);
       }
       if (count) {
-        console.log(count, "count from the register");
-
         setCookie("pgs_bskt_cnt", count, 7);
       }
       if (sessionId) {
-        console.log(sessionId, "sessionId from the register");
-
         setCookie("pgs_x", sessionId, 7);
       }
     }
@@ -38,30 +33,23 @@ const GoogleOneTapPgs = () => {
 
   function extractDetails(text: any) {
     const parts = text.toString().split("##SPLIT##");
-    console.log(parts, "split parts --------");
     if (parts) {
       const initial = parts[3] || "";
       const favCount = parts[4] || "";
       const sessionId = parts[5] || "";
       if (initial) {
-        console.log(initial, "initila fomr the login");
         setCookie("pgs_auth", initial, 7);
       }
       if (favCount) {
-        console.log(favCount, "favCount fomr the login");
-
         setCookie("pgs_bskt_cnt", favCount, 7);
       }
       if (sessionId) {
-        console.log(sessionId, "sessionId fomr the login");
-
         setCookie("pgs_x", sessionId, 7);
       }
     }
     window.location.href = "/";
   }
   function setCookie(name: string, value: string, days: number) {
-    console.log("inside the setcookies function ");
     const d = new Date();
     d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
     const expires = "expires=" + d.toUTCString();
@@ -70,7 +58,6 @@ const GoogleOneTapPgs = () => {
   async function fetchData(credential: any) {
     try {
       const decodedToken: any = jwtDecode(credential.toString());
-      console.log(decodedToken, "decodedtoken in callback");
       const url = `pgs/pgs_interactive.check_email_exist_prc?p_email=${decodedToken?.email}`;
 
       const response = await fetch(url, {
@@ -84,7 +71,6 @@ const GoogleOneTapPgs = () => {
 
       if (contentType && contentType.includes("text/html")) {
         const htmlResponse = await response.text();
-        console.log("HTML Response:", htmlResponse);
 
         if (htmlResponse.trim().toLowerCase() === "y") {
           // Second fetch if the response is 'Y'
@@ -98,10 +84,8 @@ const GoogleOneTapPgs = () => {
             }
           );
           const loginData = await loginResponse.text();
-          console.log("Login Response:", loginData);
           extractDetails(loginData);
           // const session_id = extractLastNumber(loginData);
-          // console.log(session_id, "session id wet get from the responce");
           // If session_id exists, set it in cookies
           // if (session_id) {
           //   setCookie("pgs_x", session_id, 7); // Expires in 7 days
@@ -118,11 +102,9 @@ const GoogleOneTapPgs = () => {
           );
           const loginData = await loginResponse.text();
           extractdetailsforregister(loginData);
-          console.log(loginData, "login data from the registraion api");
         }
       } else {
         const data = await response.json();
-        console.log("API Response:", data);
       }
     } catch (error) {
       console.error("Error fetching the email data:", error);
@@ -132,7 +114,8 @@ const GoogleOneTapPgs = () => {
     async function checkSession() {
       try {
         const user_initial = getCookieValue("pgs_auth") || "";
-        if (user_initial !== "null" && user_initial) {
+        const pgs_x = getCookieValue("pgs_x") || "";
+        if (user_initial !== "null" && user_initial && pgs_x) {
           return null;
         } else {
           watchForOptanonCookie();
@@ -166,7 +149,6 @@ const GoogleOneTapPgs = () => {
             "1007776276005-306decchqtue4pjqffn8qidnphccg2km.apps.googleusercontent.com",
           callback: (response: any) => {
             const { credential } = response;
-            console.log(credential);
             fetchData(credential);
           },
         });
