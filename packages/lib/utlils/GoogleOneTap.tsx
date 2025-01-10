b06ccd418f5f209506ffa5a48398692ed0513651
Manirathinam K <manirathinam.k@idp.com>
2025-01-10 08:15:25 +0530

    #feature/Hew-508#comments-added the api call for the onetap

--- packages/lib/utlils/GoogleOneTap.tsx
+++ packages/lib/utlils/GoogleOneTap.tsx
@@ -2,11 +2,64 @@
 import { useEffect, use } from "react";
 import { fetchAuthSession } from "aws-amplify/auth";
 import { signInWithRedirect } from "aws-amplify/auth";
+import { v4 as uuidv4 } from "uuid";
 const GoogleOneTap = () => {
   const scriptId = "google-one-tap-script";
   const scriptSrc = "https://accounts.google.com/gsi/client";
 
   useEffect(() => {
+    async function watchForCognitoCookie() {
+      let previousCookies = document.cookie;
+      const randomid = uuidv4();
+      const observer = new MutationObserver(async () => {
+        if (document.cookie !== previousCookies) {
+          previousCookies = document.cookie;
+
+          if (document.cookie.includes("CognitoIdentityServiceProvider")) {
+            console.log("call that");
+
+            try {
+              const session = await fetchAuthSession();
+              const response = await fetch(
+                `${process.env.NEXT_PUBLIC_BFF_API_DOMAIN}/hewebsites/v1/users/registration`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "x-api-key": `${process.env.NEXT_PUBLIC_X_API_KEY}`,
+                  },
+                  body: JSON.stringify({
+                    params: {
+                      header: {
+                        siteCode: "WU_APP",
+                        "x-correlation-id": randomid,
+                        authorization: session?.tokens?.idToken,
+                      },
+                    },
+                  }),
+                }
+              );
+
+              if (!response.ok) {
+                console.error("Failed to register user:", response.statusText);
+              } else {
+                console.log("User registered successfully");
+              }
+            } catch (error) {
+              console.error("Error during user registration:", error);
+            } finally {
+              observer.disconnect(); // Stop observing once cookie is processed
+            }
+          }
+        }
+      });
+
+      // Start observing
+      observer.observe(document, {
+        subtree: true,
+        childList: true,
+      });
+    }
     const checkSession = async () => {
       try {
         const session = await fetchAuthSession();
@@ -53,6 +106,7 @@ const GoogleOneTap = () => {
               provider: "Google",
               customState: "home page", // You can pass the credential as custom state if needed
             });
+            watchForCognitoCookie();
             const { credential } = response;
           },
         });
