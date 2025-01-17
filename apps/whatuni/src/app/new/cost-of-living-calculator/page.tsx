b709a22729c31b33c7a8da91e64c07b750fe7ecc
Udhaya kumar <udhayakumar.d@idp.com>
2025-01-17 18:37:08 +0530

    Update page.tsx

--- apps/whatuni/src/app/new/cost-of-living-calculator/page.tsx
+++ apps/whatuni/src/app/new/cost-of-living-calculator/page.tsx
@@ -1,12 +1,10 @@
-"use server";
+"use client";
 import React from "react";
 import MicroFrontend from "./micro-frontend";
-import Subscribecomponents from "@packages/shared-components/article-landing/subscribe-newsletter/subscribecomponents";
 const page = () => {
   return (
     <>
       <MicroFrontend />
-      <Subscribecomponents isContentPreview={false} />
     </>
   );
 };
