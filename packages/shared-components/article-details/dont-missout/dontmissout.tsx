bb2fca2aec26c24a3bedafab6b889e9560e50e26
Manirathinam K <manirathinam.k@idp.com>
2025-01-26 20:22:33 +0530

    #feature/new-a#comments-added error.tsx and chaged the logic for the newsletter

--- packages/shared-components/article-details/dont-missout/dontmissout.tsx
+++ packages/shared-components/article-details/dont-missout/dontmissout.tsx
@@ -33,13 +33,9 @@ const Dontmissout = ({ key, data, preview }: any) => {
   const [agreementerror, setAgreementerror] = useState(false);
   const [successMessage, setSuccessMessage] = useState(false);
   const validateEmail = (email: any) => {
-    // Simple email regex validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    // console.log(emailRegex.test(email), "weewewewewewewewew");
     return emailRegex.test(email);
   };
-
   const{category, subCategory, articleTitle} = getArticleDetailUrlParamValues();
 
   useEffect(() => {
@@ -98,7 +94,6 @@ const Dontmissout = ({ key, data, preview }: any) => {
     if (!firstname) {
       setFristnameerror(true);
     }
-
     if (!lastname) {
       isFormValid = false;
       setLastnameerror(true);
@@ -109,10 +104,8 @@ const Dontmissout = ({ key, data, preview }: any) => {
     }
     if (email) {
       if (!validateEmail(email)) {
-        // console.log("aaaaaaaaaaaaaaaaaaaaaaainside the validate email");
         setValidemailerror(true);
         isFormValid = false;
-        // console.log(validemailerror);
       }
     }
     if (!year) {
@@ -123,8 +116,6 @@ const Dontmissout = ({ key, data, preview }: any) => {
       setAgreementerror(true);
       isFormValid = false;
     }
-
-    // If all fields are valid, show success message
 
     console.log(firstname, lastname, email, year, agreement);
 
@@ -200,38 +191,18 @@ const Dontmissout = ({ key, data, preview }: any) => {
       agreement &&
       prevemail !== email
     ) {
-      // console.log(
-      //   !firstnameerror,
-      //   !lastnameerror,
-      //   !emailerror,
-      //   !yearerror,
-      //   !agreementerror,
-      //   !validemailerror,
-      //   firstname,
-      //   lastname,
-      //   email,
-      //   year,
-      //   agreement,
-      //   prevemail
-      // );
-      // console.log(!validemailerror);
+      setSuccessMessage(true);
       res()
         .then(async(response) => {
-
           if (response.ok) {
             setSuccessMessage(true);
             const resdata = await response.json();
-            //console.log("resdata: ", resdata);
             handleSubscriptionGAlog();
             handleSubscriptionCSlog(resdata?.user_uuid ?? "0", "");
-          } else {
-            throw new Error("Response not OK"); // Handle non-OK responses
-          }
+          } 
         })
-        .then((data) => console.log(data)) // Log data if response is OK
         .catch((error) => {
           console.error("Error:", error); // Handle any errors
-          setSuccessMessage(false); // Optionally set success to false on error
           handleSubscriptionCSlog("0", error.toString());
         });
     } else if (prevemail && prevemail === email) {
