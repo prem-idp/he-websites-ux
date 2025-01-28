a266f9ece6efb4a11d7c50865627a0ec8292fb0c
Manirathinam K <manirathinam.k@idp.com>
2025-01-28 16:07:19 +0530

    #feature/new-a#comments-added the redirection

--- apps/whatuni/src/middleware.ts
+++ apps/whatuni/src/middleware.ts
@@ -2,15 +2,24 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname, search } = request.nextUrl;
+  const customDomain=`${process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "https://mdev.dev.aws.whatuni.com" : process.env.NEXT_PUBLIC_ENVIRONMENT === "stg" ? "https://mtest.test.aws.whatuni.com" : process.env.NEXT_PUBLIC_ENVIRONMENT === "prd" ? "https://www.whatuni.com" : "http://localhost:3000" }`;
+
   if (search && pathname.endsWith("/")) {
+   
     const newUrl = pathname.slice(0, -1) + search;
-    return NextResponse.rewrite(new URL(newUrl, request.url));
+    return NextResponse.redirect(new URL(newUrl, customDomain));
   }
   if (!search && !pathname.endsWith("/")) {
-    return NextResponse.rewrite(new URL(`${pathname}/`, request.url));
+    
+
+    return NextResponse.redirect(new URL(`${pathname}/`, customDomain));
   }
   return NextResponse.next();
 }
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|fonts|images|icons).*)",
+  ],
 };
+
+
