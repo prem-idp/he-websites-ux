2761fb6e1919071e4e4a167669af9dc90c6075fa
Ramkumar M <ramkumar.manokaran@idp.com>
2025-01-29 14:42:55 +0530

    dontmissot update fontsize

--- packages/shared-components/common-utilities/newsletter-and-subscription/dont-missout/dontmissout.tsx
+++ packages/shared-components/common-utilities/newsletter-and-subscription/dont-missout/dontmissout.tsx
@@ -574,9 +574,9 @@ const Dontmissout = ({ key, data, preview }: any) => {
                       </div>
                       <label
                         htmlFor="newsletters"
-                        className="select-none check-label small font-semibold w-[calc(100%_-_28px)]"
+                        className="select-none check-label !text-small font-semibold w-[calc(100%_-_28px)]"
                       >
-                        <p className="select-none small font-normal text-grey-600">
+                        <p className="select-none !text-small font-normal text-grey-600">
                           I confirm I’m over 13 and agree to the
                           <Link
                             className="!text-primary-500 underline pl-[4px]"
