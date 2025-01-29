d099ae0f1c6d01f2b3cc1c7b760355a5c8ecd501
Manirathinam K <manirathinam.k@idp.com>
2025-01-29 16:03:46 +0530

    #feature/new-a#comments-changed strategy

--- packages/lib/oneTrust/OneTrustCookieScript.tsx
+++ packages/lib/oneTrust/OneTrustCookieScript.tsx
@@ -144,8 +144,8 @@ export default function OneTrustCookieScript({
           {userConsentGiven ? (
             <Script
               src="https://accounts.google.com/gsi/client"
-              id="googleGsiId"
-              strategy="lazyOnload"
+              id="googleGsiId"npm 
+              strategy="beforeInteractive"
             />
           ) : (
             <>
@@ -153,7 +153,7 @@ export default function OneTrustCookieScript({
                 src={`${process.env.NEXT_PUBLIC_ONE_TRUST_SRC}`}
                 id="oneTrustCookieeId"
                 data-domain-script={domianValue}
-                strategy="lazyOnload"
+                strategy="beforeInteractive"
               />
             </>
           )}
