d143404b91445d23070bbff5e8b6f97585d1aabb
Udhaya kumar <udhayakumar.d@idp.com>
2025-02-19 16:28:21 +0530

    Update srpage-component.tsx

--- packages/shared-components/sr-page/srpage-component.tsx
+++ packages/shared-components/sr-page/srpage-component.tsx
@@ -18,76 +18,108 @@ import { headers } from "next/headers";
 import { getQualCode, getSearchPayload } from "../services/utils";
 import { getDecodedCookie } from "@packages/lib/utlils/result-filters";
 
-
-
-const SearchResultComponent = async ({ searchparams, pathname}: any) => {
+const SearchResultComponent = async ({ searchparams, pathname }: any) => {
   //const userRegion = headerlist?.get('cloudfront-viewer-country-region');
- // const filterCookie = getCookieValue("filter_param");
+  // const filterCookie = getCookieValue("filter_param");
   //const filterCookieParam = filterCookie ? JSON.parse(filterCookie) : null;
   console.log("query" + pathname);
   const headersList = await headers();
   const referer = headersList.get("referer");
-  const pathnameArray = (referer?.split?.("/"));
+  const pathnameArray = referer?.split?.("/");
   let searchResultsData;
   let filterCookieParam;
-  if (typeof document !== 'undefined') {
-    const filterCookieParam = JSON.parse(getDecodedCookie("filter_param") || "{}");
-}
+  if (typeof document !== "undefined") {
+    const filterCookieParam = JSON.parse(
+      getDecodedCookie("filter_param") || "{}"
+    );
+  }
   try {
-    searchResultsData = await searchResultsFetchFunction(getSearchPayload(searchparams,filterCookieParam,pathnameArray?.[3]?.split?.("-")?.[0]));
+    searchResultsData = await searchResultsFetchFunction(
+      getSearchPayload(
+        searchparams,
+        filterCookieParam,
+        pathnameArray?.[3]?.split?.("-")?.[0]
+      )
+    );
     console.log(
       "After fetching search results" + JSON.stringify(searchResultsData)
     );
-  } catch(error) {
+  } catch (error) {
     console.log("error", error);
   }
 
   return (
     <>
       <TopSection />
-      {searchResultsData?.searchResultsList ? 
       <Suspense>
         <SearchFilterButtons />
         <SearchLabels />
-      </Suspense> : <></>}
+      </Suspense>
+      {searchResultsData?.searchResultsList ? (
+        <Suspense>
+          <SearchFilterButtons />
+          <SearchLabels />
+        </Suspense>
+      ) : (
+        <></>
+      )}
       <section className="p-[16px] md:px-[20px] lg:pt-[16px] xl:px-0">
-
-        <div className="max-w-container mx-auto">   
-          
-          {searchResultsData?.searchResultsList ? 
-          <>
-           <GradeBanner />
-           <SortingFilter sortParam={searchparams?.sort}/>   
-          {searchResultsData?.featuredProviderDetails && searchResultsData?.featuredProviderDetails?.collegeId !== 0 ?
-          <FeaturedVideoSection featuredData={searchResultsData?.featuredProviderDetails}/> : <></>}
-          <SrPageResultPod
-            searchResultsData={searchResultsData?.searchResultsList}
-          />
-           {searchResultsData?.collegeCount > 10? 
-           <Paginations
-            totalPages={Math.ceil(searchResultsData?.collegeCount / 10)} currentPage={searchparams?.pageNo || 1}
-          /> : <></>}</> : <><SrPageNoResults/></>}         
-        </div>
-      </section>
-      {searchResultsData?.searchResultsList ? <>
-      <section className="bg-white px-[16px] md:px-[20px] xl:px-0">
         <div className="max-w-container mx-auto">
-          <div className="h1 pt-[40px]">Explore more about law</div>
-          <div className="flex flex-col gap-[40px] md:gap-[80px] py-[40px]">
-            <ExploreArticles />
-            <ExploreArticles />
-          </div>
+          {searchResultsData?.searchResultsList ? (
+            <>
+              <GradeBanner />
+              <SortingFilter sortParam={searchparams?.sort} />
+              {searchResultsData?.featuredProviderDetails &&
+              searchResultsData?.featuredProviderDetails?.collegeId !== 0 ? (
+                <FeaturedVideoSection
+                  featuredData={searchResultsData?.featuredProviderDetails}
+                />
+              ) : (
+                <></>
+              )}
+              <SrPageResultPod
+                searchResultsData={searchResultsData?.searchResultsList}
+              />
+              {searchResultsData?.collegeCount > 10 ? (
+                <Paginations
+                  totalPages={Math.ceil(searchResultsData?.collegeCount / 10)}
+                  currentPage={searchparams?.pageNo || 1}
+                />
+              ) : (
+                <></>
+              )}
+            </>
+          ) : (
+            <>
+              <SrPageNoResults />
+            </>
+          )}
         </div>
       </section>
-      <Faqcomponents />
-      <ContentfulPreviewProvider
-        locale="en-GB"
-        enableInspectorMode={false}
-        enableLiveUpdates={false}
-        debugMode={false}
-      >
-        <Subscribecomponents iscontentPreview={false} />
-      </ContentfulPreviewProvider></> : <></>}
+      {searchResultsData?.searchResultsList ? (
+        <>
+          <section className="bg-white px-[16px] md:px-[20px] xl:px-0">
+            <div className="max-w-container mx-auto">
+              <div className="h1 pt-[40px]">Explore more about law</div>
+              <div className="flex flex-col gap-[40px] md:gap-[80px] py-[40px]">
+                <ExploreArticles />
+                <ExploreArticles />
+              </div>
+            </div>
+          </section>
+          <Faqcomponents />
+          <ContentfulPreviewProvider
+            locale="en-GB"
+            enableInspectorMode={false}
+            enableLiveUpdates={false}
+            debugMode={false}
+          >
+            <Subscribecomponents iscontentPreview={false} />
+          </ContentfulPreviewProvider>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
