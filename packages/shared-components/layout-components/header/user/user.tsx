9015e3070fe9a6858db31efbda8c4e5e6a3c87f2
Jeevitha Ravi <jeevitha.ravi@idp.com>
2025-02-21 14:59:45 +0530

    Update user.tsx

--- packages/shared-components/layout-components/header/user/user.tsx
+++ packages/shared-components/layout-components/header/user/user.tsx
@@ -11,6 +11,7 @@ export default function User({ topnav_data }: any) {
     if (process.env.PROJECT === "Whatuni") {
       try {
         sessionStorage.clear();
+        localStorage?.removeItem("resultSubmit")
         document.cookie =
         "LogedinviaOnetap=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
         document.cookie =
