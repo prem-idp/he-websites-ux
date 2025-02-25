a7576f480abad37297b9038849a62fdb7af9d384
Udhaya kumar <udhayakumar.d@idp.com>
2025-02-25 17:47:56 +0530

    #HEW-1106  #commments updated the code

--- packages/shared-components/common-utilities/popups/searchfiltercomponent.tsx
+++ packages/shared-components/common-utilities/popups/searchfiltercomponent.tsx
@@ -264,6 +264,21 @@ const SearchFilterComponent = ({ jsondata, path }: any) => {
     ?.filter(Boolean);
   const set: any = new Set(totalFilter);
   const ar = [...set];
+
+  const FirstLevelRegion = jsondata?.regionList
+    ?.map((region: any) => {
+      if (region.parentRegionId == 1) return region;
+    })
+    .filter(Boolean);
+
+  console.log(FirstLevelRegion);
+  // console.log(
+  //   jsondata?.regionList
+  //     ?.map((region: any) => {
+  //       if (region.parentRegionId == ) return region;
+  //     })
+  //     .filter(Boolean)
+  // );
   return (
     <>
       <div>
@@ -905,79 +920,81 @@ const SearchFilterComponent = ({ jsondata, path }: any) => {
                         </div>
                         <ul>
                           <li>
-                            {jsondata?.regionList?.map(
-                              (item: any, index: any) => (
-                                <div key={index}>
-                                  <div className="form_check relative m-[0_0_12px_24px]">
-                                    <div className="flex items-start gap-[8px]">
-                                      <div className="checkbox_card">
-                                        <input
-                                          onClick={() => {
-                                            appendSearchParams(
+                            {FirstLevelRegion?.map((item: any, index: any) => (
+                              <div key={index}>
+                                <div className="form_check relative m-[0_0_12px_24px]">
+                                  <div className="flex items-start gap-[8px]">
+                                    <div className="checkbox_card">
+                                      <input
+                                        onClick={() => {
+                                          appendSearchParams(
+                                            "location",
+                                            item?.regionTextKey
+                                          );
+                                        }}
+                                        type="checkbox"
+                                        className="form-checkbox hidden"
+                                        id={item?.regionName}
+                                        name={item?.regionName}
+                                      />
+                                      {isIndexed && (
+                                        <Link
+                                          id={"location" + item?.regionTextKey}
+                                          href={{
+                                            pathname: `${slug}`,
+                                            query: formUrl(
                                               "location",
                                               item?.regionTextKey
-                                            );
+                                            ),
                                           }}
-                                          type="checkbox"
-                                          className="form-checkbox hidden"
-                                          id={item?.regionName}
-                                          name={item?.regionName}
-                                        />
-                                        {isIndexed && (
-                                          <Link
-                                            id={
-                                              "location" + item?.regionTextKey
-                                            }
-                                            // href={formUrl(
-                                            //   "location",
-                                            //   item?.regionTextKey
-                                            // )}
-                                            href={{
-                                              pathname: `${slug}`,
-                                              query: formUrl(
-                                                "location",
-                                                item?.regionTextKey
-                                              ),
-                                            }}
-                                          ></Link>
-                                        )}
-                                        <label
-                                          htmlFor={item?.regionName}
-                                          className="flex justify-center items-center w-[16px] h-[16px] rounded-[3px] border-2 border-grey-600 my-[2px] group-checked:bg-primary-400"
-                                        >
-                                          <svg
-                                            width="10"
-                                            height="8"
-                                            viewBox="0 0 10 8"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                          >
-                                            <path
-                                              fillRule="evenodd"
-                                              clipRule="evenodd"
-                                              d="M9.2534 0.723569C9.40607 0.863517 9.41638 1.10073 9.27643 1.2534L3.77643 7.2534C3.70732 7.3288 3.6104 7.37269 3.50815 7.37491C3.40589 7.37714 3.30716 7.33749 3.23483 7.26517L0.734835 4.76517C0.588388 4.61872 0.588388 4.38128 0.734835 4.23484C0.881282 4.08839 1.11872 4.08839 1.26517 4.23484L3.48822 6.45789L8.72357 0.746605C8.86351 0.593936 9.10073 0.583622 9.2534 0.723569Z"
-                                              fill="white"
-                                              stroke="white"
-                                              strokeWidth="0.666667"
-                                              strokeLinecap="round"
-                                              strokeLinejoin="round"
-                                            />
-                                          </svg>
-                                        </label>
-                                      </div>
+                                        ></Link>
+                                      )}
                                       <label
                                         htmlFor={item?.regionName}
-                                        className="check-label small font-normal text-grey300 w-[calc(100%_-_28px)]"
+                                        className="flex justify-center items-center w-[16px] h-[16px] rounded-[3px] border-2 border-grey-600 my-[2px] group-checked:bg-primary-400"
                                       >
-                                        {item?.regionName}
+                                        <svg
+                                          width="10"
+                                          height="8"
+                                          viewBox="0 0 10 8"
+                                          fill="none"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                          <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M9.2534 0.723569C9.40607 0.863517 9.41638 1.10073 9.27643 1.2534L3.77643 7.2534C3.70732 7.3288 3.6104 7.37269 3.50815 7.37491C3.40589 7.37714 3.30716 7.33749 3.23483 7.26517L0.734835 4.76517C0.588388 4.61872 0.588388 4.38128 0.734835 4.23484C0.881282 4.08839 1.11872 4.08839 1.26517 4.23484L3.48822 6.45789L8.72357 0.746605C8.86351 0.593936 9.10073 0.583622 9.2534 0.723569Z"
+                                            fill="white"
+                                            stroke="white"
+                                            strokeWidth="0.666667"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                          />
+                                        </svg>
                                       </label>
                                     </div>
+                                    <label
+                                      htmlFor={item?.regionName}
+                                      className="check-label small font-normal text-grey300 w-[calc(100%_-_28px)]"
+                                    >
+                                      {item?.regionName}
+                                    </label>
                                   </div>
-                                  {/* <ul>
-                                  {index == 0 && (
-                                    <li className="grid grid-flow-row md:grid-rows-8 md:grid-flow-col">
-                                      {jsondata?.regionList.map(
-                                        (item: any, index: any) => (
+                                </div>
+                                <ul>
+                                  <li className="grid grid-flow-row md:grid-rows-8 md:grid-flow-col">
+                                    {jsondata?.regionList
+                                      ?.map((regionlist: any) => {
+                                        if (
+                                          regionlist?.parentRegionId ==
+                                          item?.regionId
+                                        ) {
+                                          return regionlist;
+                                        }
+                                      })
+                                      .filter(Boolean)
+                                      ?.map((childItem: any, index: any) => (
+                                        <div key={index}>
                                           <div
                                             className="form_check relative m-[0_0_12px_40px]"
                                             key={index}
@@ -987,10 +1004,12 @@ const SearchFilterComponent = ({ jsondata, path }: any) => {
                                                 <input
                                                   type="checkbox"
                                                   className="form-checkbox hidden"
-                                                  id={item?.regionName}
+                                                  id={childItem?.regionName}
                                                 />
                                                 <label
-                                                  htmlFor={item?.regionName}
+                                                  htmlFor={
+                                                    childItem?.regionName
+                                                  }
                                                   className="flex justify-center items-center w-[16px] h-[16px] rounded-[3px] border-2 border-grey-600 my-[2px] group-checked:bg-primary-400"
                                                 >
                                                   <svg
@@ -1014,21 +1033,19 @@ const SearchFilterComponent = ({ jsondata, path }: any) => {
                                                 </label>
                                               </div>
                                               <label
-                                                htmlFor={item?.regionName}
+                                                htmlFor={childItem?.regionName}
                                                 className="check-label small font-normal text-grey300 w-[calc(100%_-_28px)]"
                                               >
-                                                {item?.regionName}hello
+                                                {childItem?.regionName}
                                               </label>
                                             </div>
                                           </div>
-                                        )
-                                      )}
-                                    </li>
-                                  )}
-                                </ul> */}
-                                </div>
-                              )
-                            )}
+                                        </div>
+                                      ))}
+                                  </li>
+                                </ul>
+                              </div>
+                            ))}
                           </li>
                         </ul>
                       </li>
