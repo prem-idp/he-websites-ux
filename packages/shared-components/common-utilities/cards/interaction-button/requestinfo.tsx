0ef2c5008c28f5f226080f4a16316ffbcd878b78
Rahul J <rahul.j@idp.com>
2025-03-04 12:43:23 +0530

    Button logic changes

--- packages/shared-components/common-utilities/cards/interaction-button/requestinfo.tsx
+++ packages/shared-components/common-utilities/cards/interaction-button/requestinfo.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import router from "next/router";
+import { fetchenquirydata } from '@packages/lib/server-actions/server-action';
 
-const RequestInfo = ({ showCount }: any) => {
+const RequestInfo = ({ enquiryProps }: any) => {
+  const handleRequestInfo = async () => {
+    console.log("Enter", enquiryProps);
+     try {
+      const riPayload = {
+        suborderItemId: enquiryProps?.subOrderItemid,
+        orderItemId: enquiryProps?.orderItemId,
+        collegeId: enquiryProps?.collegeId,
+        affiliateId: 220703,
+        sponsoredListingFlag: enquiryProps?.sponsoredListingFlag,
+        //manualBoostingFlag: enquiryProps?.manualBoostingFlag,
+      };
+      const response = await fetchenquirydata(riPayload);
+      console.log("response printing", response);
+      if(response?.requestInfoWebform){
+        const url = `/degrees/navigation-url.html?id=${enquiryProps?.subOrderItemid || ""}&cta-button-name=email_webform`;
+        console.log("URL Printing", url);
+        router.push(url);
+      }
+      if(enquiryProps?.pageName === "browsemoneypageresults" && response?.requestInfoEmail){
+        const url = `/degrees/email${enquiryProps?.collegeName ? `/${enquiryProps?.collegeName}`:""}${enquiryProps?.collegeId ? `/${enquiryProps?.collegeId}`:""}${enquiryProps?.courseId ? `/${enquiryProps?.courseId}`:""}/law${enquiryProps?.subOrderItemid ? `/${enquiryProps?.subOrderItemid}`:""}/send-college-email.html?sponsoredOrderItemId=${enquiryProps?.sponsoredListingFlag || "0"}&manualBoostingFlag=${enquiryProps?.manualBoostingFlag || "N"}&pageName=sr&fromPage=${enquiryProps?.pageName}`;
+        console.log("URL Printing", url);
+        router.push(url);
+      }
+      if(enquiryProps?.pageName === "coursesearchresult" && response?.requestInfoEmail){
+        const url = `/degrees/email${enquiryProps?.collegeName ? `/${enquiryProps?.collegeName}`:""}${enquiryProps?.collegeId ? `/${enquiryProps?.collegeId}`:""}${enquiryProps?.courseId ? `/${enquiryProps?.courseId}`:""}/law${enquiryProps?.subOrderItemid ? `/${enquiryProps?.subOrderItemid}`:""}/send-college-email.html?fromPage=${enquiryProps?.pageName}`;
+        console.log("URL Printing", url);
+        router.push(url);
+      }
+     } catch(error){
+        console.error('Error fetching enquiry data:', error);
+     }
+  };
   return (
     <>
       <button
         type="button"
-        className={`btn btn-blue w-full ${
-          showCount == 1 ? "justify-self-end lg:w-[162px]" : ""
-        }`}
-      >
+        className="btn btn-blue w-full" onClick={handleRequestInfo}>
         Request info
       </button>
     </>
--- packages/shared-components/common-utilities/cards/interaction-button/visitwebsite.tsx
+++ packages/shared-components/common-utilities/cards/interaction-button/visitwebsite.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { fetchenquirydata } from '@packages/lib/server-actions/server-action';
 
-const Visitwebsite = ({ pageName, showCount }: any) => {
+const Visitwebsite = ({ enquiryProps }: any) => {
+  const handleVisitWebsite = async () => {
+    console.log("Enter", enquiryProps);
+     try {
+      const vwPayload = {
+        suborderItemId: enquiryProps?.subOrderItemid,
+        orderItemId: enquiryProps?.orderItemId || null,
+        collegeId: enquiryProps?.collegeId,
+        affiliateId: 220703,
+        sponsoredListingFlag: enquiryProps?.sponsoredListingFlag || null,
+        //manualBoostingFlag: enquiryProps?.manualBoostingFlag,
+      };
+      console.log("payload", vwPayload);
+      const response = await fetchenquirydata(vwPayload);
+      console.log("response printing", response);
+      if(response?.website){
+        console.log("VWURL", response?.website);
+        window.open(response.website, '_blank');
+      }
+     } catch(error){
+        console.error('Error fetching enquiry data:', error);
+     }
+  };
   return (
     <button
-      type="button" className={`btn btn-grey w-full ${ showCount == 4 ? "md:col-start-2" : "" }`}
+      type="button" className="btn btn-grey w-full" onClick={handleVisitWebsite}
     >
       Visit website
     </button> 
-  )
-}
+  );
+};
 
 export default Visitwebsite
