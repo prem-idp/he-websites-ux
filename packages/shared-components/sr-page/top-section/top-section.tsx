adb9dca9350d9da1eeb4a916f89b2480597699d4
Udhaya kumar <udhayakumar.d@idp.com>
2025-03-05 14:52:17 +0530

    #HEW-1286 #comments pipeline fixed

--- packages/shared-components/sr-page/top-section/top-section.tsx
+++ packages/shared-components/sr-page/top-section/top-section.tsx
@@ -1,31 +1,48 @@
 import React from "react";
 import Breadcrumblayoutcomponent from "@packages/shared-components/common-utilities/breadcrumb-layout/breadcrumblayoutcomponent";
 import TopSectionSkeleton from "@packages/shared-components/skeleton/search-result/top-section-skeleton";
-import { Seosrdetails } from "@packages/lib/graphQL/graphql-query";
-import { graphQlFetchFunction, httpBFFRequest } from "@packages/lib/server-actions/server-action";
-import { SEOparams } from "@packages/lib/types/interfaces";
-import { getDisplayNameReqBody, getSeoMetaFeildId, replaceSEOPlaceHolder } from "@packages/lib/utlils/resultsPageActions"
+//import { Seosrdetails } from "@packages/lib/graphQL/graphql-query";
+import {
+  graphQlFetchFunction,
+  httpBFFRequest,
+} from "@packages/lib/server-actions/server-action";
+//import { SEOparams } from "@packages/lib/types/interfaces";
+import {
+  getDisplayNameReqBody,
+  getSeoMetaFeildId,
+  replaceSEOPlaceHolder,
+} from "@packages/lib/utlils/resultsPageActions";
 import { getMetaDetailsQueryForSRpage } from "@packages/lib/graphQL/search-results";
 interface searchProps {
   searchParam?: any;
   searchResultsData: any[];
 }
-interface MetaFilterTypesReplace{
-  searchSubject?: string[],
-  studylevel?: string,
-  studymode?: string,
-  location?: string[],
-  providerCount?: string,
-  courseCount?: any,
+interface MetaFilterTypesReplace {
+  searchSubject?: string[];
+  studylevel?: string;
+  studymode?: string;
+  location?: string[];
+  providerCount?: string;
+  courseCount?: any;
 }
-const TopSection: React.FC<searchProps> = async({ searchParam, searchResultsData }) => {
+const TopSection: React.FC<searchProps> = async ({
+  searchParam,
+  searchResultsData,
+}) => {
   const displayNameReqBody = getDisplayNameReqBody(searchParam);
   const displayNameBFFEndPt = `${process.env.NEXT_PUBLIC_BFF_API_DOMAIN}/hewebsites/v1/seo/search-display-names`;
-  const displayNameResponse = await httpBFFRequest(displayNameBFFEndPt, displayNameReqBody, "POST", `${process.env.NEXT_PUBLIC_X_API_KEY}`, "default");
+  const displayNameResponse = await httpBFFRequest(
+    displayNameBFFEndPt,
+    displayNameReqBody,
+    "POST",
+    `${process.env.NEXT_PUBLIC_X_API_KEY}`,
+    "default"
+  );
   const seoMetaFeildId: string = getSeoMetaFeildId(searchParam);
-   const query = getMetaDetailsQueryForSRpage(seoMetaFeildId);
-    let contentfulMetadata = await graphQlFetchFunction(query);
-    contentfulMetadata = contentfulMetadata?.data?.pageSeoFieldsCollection?.items[0];
+  const query = getMetaDetailsQueryForSRpage(seoMetaFeildId);
+  let contentfulMetadata = await graphQlFetchFunction(query);
+  contentfulMetadata =
+    contentfulMetadata?.data?.pageSeoFieldsCollection?.items[0];
 
   const breadcrumbData = [
     {
@@ -47,11 +64,11 @@ const TopSection: React.FC<searchProps> = async({ searchParam, searchResultsData
   ];
   const metaFiltersOpted: MetaFilterTypesReplace = {
     courseCount: searchResultsData.length ?? undefined,
-    location:  displayNameResponse?.locationName ?? undefined,
+    location: displayNameResponse?.locationName ?? undefined,
     searchSubject: displayNameResponse?.subjectName ?? undefined,
     studylevel: displayNameResponse?.studyLevel ?? undefined,
     studymode: displayNameResponse?.studyMode ?? undefined,
-  }
+  };
   return (
     <>
       {/* start breadcrumb and subject*/}
@@ -65,11 +82,18 @@ const TopSection: React.FC<searchProps> = async({ searchParam, searchResultsData
           {/* start subject */}
           <div className="py-[16px]">
             <div className="h5 mb-[4px]">
-            {
-            replaceSEOPlaceHolder(contentfulMetadata?.h1Title, metaFiltersOpted) }
+              {replaceSEOPlaceHolder(
+                contentfulMetadata?.h1Title,
+                metaFiltersOpted
+              )}
             </div>
-            <p> {
-            replaceSEOPlaceHolder(contentfulMetadata?.h2Text, metaFiltersOpted)}</p>
+            <p>
+              {" "}
+              {replaceSEOPlaceHolder(
+                contentfulMetadata?.h2Text,
+                metaFiltersOpted
+              )}
+            </p>
           </div>
           {/* end subject */}
           {/* <TopSectionSkeleton/> */}
