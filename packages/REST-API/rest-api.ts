64064b8a4427e8bbc3d90e16440b705658caa717
Udhaya kumar <udhayakumar.d@idp.com>
2025-03-11 14:59:07 +0530

    #HEW-1106 #comments updated the user yoy details

--- packages/REST-API/rest-api.ts
+++ packages/REST-API/rest-api.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import { fetchAuthSession } from "@aws-amplify/auth";
 const makeApiCall = async (
   url: string,
   method: string,
@@ -107,5 +108,39 @@ const getSrFilterCount = async (bodyjson: any): Promise<any> => {
     console.error("Unknown error occurred", { endpoint: apiUrl, error });
   }
 };
+
+const getUserYearOfEntry = async (bodyjson: any): Promise<any> => {
+  const apiUrl = `${process.env.NEXT_PUBLIC_BFF_API_DOMAIN}/dev-hewebsites-bff/v1/users/details`;
+  const response = await fetchAuthSession({ forceRefresh: true });
+  const { idToken } = response?.tokens ?? {};
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-correlation-id": uuidv4(),
+        Authorization: `${idToken}`,
+        sitecode: `${process.env.SITE_CODE}`,
+        "x-api-key": `${process.env.NEXT_PUBLIC_X_API_KEY}`,
+      },
+      body: JSON.stringify(bodyjson),
+    });
+    if (!response.ok) {
+      const errorResponse = await response.json().catch(() => ({}));
+      console.error({
+        error: errorResponse,
+        endpoint: apiUrl,
+        status: response?.status,
+        statusText: response?.statusText,
+      });
+      return null;
+    }
+    return await response.json();
+  } catch (error: unknown) {
+    console.error(`API call failed: ${error}`, { endpoint: apiUrl });
+    console.error("Unknown error occurred", { endpoint: apiUrl, error });
+  }
+};
+
 export default makeApiCall;
-export { getSrFilter, getSrFilterCount };
+export { getSrFilter, getSrFilterCount, getUserYearOfEntry };
