6968578d1dd2c64a2e8b90ae890fe8bca011be91
Rahul J <rahul.j@idp.com>
2025-03-12 14:56:18 +0530

    Webform url changes

--- packages/shared-components/common-utilities/cards/interaction-button/requestinfo.tsx
+++ packages/shared-components/common-utilities/cards/interaction-button/requestinfo.tsx
@@ -18,9 +18,11 @@ const RequestInfo = ({ enquiryProps }: any) => {
       const response = await fetchenquirydata(requestInfoPayload);
       console.log("response printing", response);
       if(response?.requestInfoWebform){
-        const url = `/degrees/navigation-url.html?id=${enquiryProps?.subOrderItemid || ""}&cta-button-name=email_webform`;
-        console.log("URL Printing", url);
-        router.push(url);
+        // const url = `/degrees/navigation-url.html?id=${enquiryProps?.subOrderItemid || ""}&cta-button-name=email_webform`;
+        // console.log("URL Printing", url);
+        // router.push(url);
+        console.log("REQUESTINFO_WEBFORMURL", response?.requestInfoWebform);
+        window.open(response.requestInfoWebform, '_blank');
       }
       else if(enquiryProps?.pageName === "browsemoneypageresults" && response?.requestInfoEmail){
         const url = `/degrees/email${enquiryProps?.collegeName ? `/${enquiryProps?.collegeName}-email`:""}${enquiryProps?.collegeId ? `/${enquiryProps?.collegeId}`:""}${enquiryProps?.courseId ? `/${enquiryProps?.courseId}`:""}/law${enquiryProps?.subOrderItemid ? `/n-${enquiryProps?.subOrderItemid}`:""}/send-college-email.html?${enquiryProps?.sponsoredListingFlag === "Y"? `sponsoredOrderItemId=${enquiryProps?.orderItemId}` : `sponsoredOrderItemId=0`}&manualBoostingFlag=${enquiryProps?.manualBoostingFlag || "N"}&pageName=sr&fromPage=${enquiryProps?.pageName}`;
