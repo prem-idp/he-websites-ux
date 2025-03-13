61f8820acaf41a45153675e5bfc4c90fa6b935c9
prem-idp <premkumar.mannu@idp.com>
2025-03-13 16:22:52 +0530

    loader for sr filter

--- packages/shared-components/sr-page/search-labels/search-labels.tsx
+++ packages/shared-components/sr-page/search-labels/search-labels.tsx
@@ -76,7 +76,7 @@ const SearchLabelsContent = ({searchLabel}:any) => {
 
   return (
     <>
-      <section className="overflow-x-auto snap-x snap-mandatory bg-white px-[16px] py-[8px] md:px-[20px] xl:px-0 md:sticky top-[69px] z-[4]">
+      <section className="overflow-x-auto snap-x snap-mandatory bg-white px-[16px] py-[8px] md:px-[20px] xl:px-0 md:sticky top-[69px] z-[3]">
         <div className="max-w-container mx-auto">
           <ul className="flex items-start gap-[8px] uppercase">
             {filterList?.map((items, index) => (
