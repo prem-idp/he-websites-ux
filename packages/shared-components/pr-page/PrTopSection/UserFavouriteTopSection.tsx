cfc2ffb6adb69888d9199bb7d4693b57693b6f92
sathish kumar v <sathishkumar.v@idp.com>
2025-03-13 10:35:10 +0530

    Issued Fixed for Components Changes

--- packages/shared-components/pr-page/PrTopSection/UserFavouriteTopSection.tsx
+++ packages/shared-components/pr-page/PrTopSection/UserFavouriteTopSection.tsx
@@ -1,29 +1,52 @@
 // components/ClientTopSection.js
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserFavourite from "@packages/shared-components/common-utilities/user-favourite/user-favourite";
+import { AuthUser, getCurrentUser } from "@aws-amplify/auth";
+import { getUserFavourites } from "@packages/lib/utlils/userfavourite";
 
 interface UserFavouriteTopSectionProps {
     collegeId: number;
     collegeName: string;
 }
 
-export default function UserFavouriteTopSection({ collegeId, collegeName }: UserFavouriteTopSectionProps) {
-    const [exceedMessage, setExceedMessage] = useState(false);
+interface Favourite {
+    fav_id: string;
+    fav_type: string;
+    fav_date?: string;
+    final_choice_id?: string | null;
+    choice_position?: number | null;
+}
 
-    const handleExceedMessage = (data: any) => {
-        setExceedMessage(data); // Update state in client component
-    };
+export default function UserFavouriteTopSection({ collegeId, collegeName }: UserFavouriteTopSectionProps) {
+
+    const [user, setUserData] = useState<AuthUser | null>(null);
+    const [favourite, setFavourite] = useState<{ favouritedList: any[] }>({ favouritedList: [] });
+    useEffect(() => {
+        // Getting favourites list when user logged in
+        async function checkUser() {
+            try {
+                const user: AuthUser = await getCurrentUser();
+                setUserData(user);
+                if (user && typeof window !== "undefined") {
+                    const favList: Favourite[] = await getUserFavourites();
+                    setFavourite({ favouritedList: favList?.map((fav) => fav?.fav_id) });
+                }
+            } catch (error) {
+                setUserData(null);
+            }
+        }
+        checkUser();
+    }, []);
 
     return (
         <>
-            <UserFavourite exceedData={handleExceedMessage}
+            <UserFavourite favourites={favourite}
                 contentId={collegeId}
                 contentName={collegeName}
                 contentType="INSTITUTION"
             />
-            {exceedMessage && <p>Exceed Message Triggered!</p>} {/* Example usage */}
         </>
     );
 }
