0398be0ac285193e1c471ecf4e6889fec518b9c2
Jeevitha Ravi <jeevitha.ravi@idp.com>
2025-03-25 11:52:37 +0530

    #Build error

--- packages/shared-components/sr-page/no-results/srpage-noresult.tsx
+++ packages/shared-components/sr-page/no-results/srpage-noresult.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Image from "next/image";
 
-const SrPageNoResults = ({searchedSubject}:any) => {
+const SrPageNoResults = () => {
   return (
     <>
       <div className="md:py-[56px]">
@@ -12,7 +12,7 @@ const SrPageNoResults = ({searchedSubject}:any) => {
               Well, that's unexpected!
             </div>
             <div className="small">
-              We couldn’t find any matches for {searchedSubject}.Not what you
+              We couldn’t find any matches for[search terms] Not what you
               expected? Try a new search term or check for typos. Let's find
               what you're looking for!
             </div>
--- packages/shared-components/sr-page/srpage-component.tsx
+++ packages/shared-components/sr-page/srpage-component.tsx
@@ -124,7 +124,7 @@ const SearchResultComponent = async ({ searchparams, params }: any) => {
             </>
           ) : (
             <>
-              <SrPageNoResults searchedSubject = {searchparams?.subject}/>
+              <SrPageNoResults/>
             </>
           )}
         </div>
