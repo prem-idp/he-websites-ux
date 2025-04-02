905f60dcf58a70a620d22a20e7dac57653723892
Ramkumar M <ramkumar.manokaran@idp.com>
2025-04-02 17:15:12 +0530

    page seetup is ready

---
+++ apps/whatuni/.next/static/chunks/apps_whatuni_src_app_university-life_page_tsx_ec798c68._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/apps_whatuni_src_app_university-life_page_tsx_ec798c68._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/apps_whatuni_src_app_university-life_page_tsx_ec798c68._.js",
+  "chunks": [
+    "static/chunks/_b51723b2._.js"
+  ],
+  "source": "dynamic"
+});
