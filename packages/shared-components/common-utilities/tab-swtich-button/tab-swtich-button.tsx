bf4b15a28199460209ccf4288d07786d19e0c9d3
Ramkumar M <ramkumar.manokaran@idp.com>
2025-04-16 10:45:34 +0530

    tab page

---
+++ packages/shared-components/common-utilities/tab-swtich-button/tab-swtich-button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TabSwtichButton = () => {
+  return (
+    <section className="bg-white border-b border-grey-200 overflow-x-auto snap-x snap-mandatory px-[16px] py-[8px] md:px-[20px] xl:px-0">
+      <div className="max-w-container mx-auto">
+        <div className="flex gap-[8px] whitespace-nowrap">
+          <button
+            type="button"
+            className="btn btn-black-outline bg-grey300 text-white"
+          >
+            University info
+          </button>
+          <button type="button" className="btn btn-black-outline">
+            Courses
+          </button>
+          <button type="button" className="btn btn-black-outline">
+            Open days
+          </button>
+          <button type="button" className="btn btn-black-outline">
+            Scholarships
+          </button>
+          <button type="button" className="btn btn-black-outline">
+            Reviews
+            <span className="bg-negative-default text-white rounded-[24px] p-[2px] ml-[8px]">
+              768
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TabSwtichButton;
