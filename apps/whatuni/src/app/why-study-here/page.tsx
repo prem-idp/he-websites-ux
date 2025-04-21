50493d3bb098a418b2a524241893195195a71cac
prem-idp <premkumar.mannu@idp.com>
2025-04-21 17:25:15 +0530

    HEW-3932 #Keystats changes

--- apps/whatuni/src/app/why-study-here/page.tsx
+++ apps/whatuni/src/app/why-study-here/page.tsx
@@ -12,7 +12,8 @@ import StudentRating from "@packages/shared-components/common-utilities/student-
 import StudentReviews from "@packages/shared-components/common-utilities/student-reviews/student-reviews";
 import TabSwitchButton from "@packages/shared-components/common-utilities/tab-switch-button/tab-switch-button";
 import RedirectionButton from "@packages/shared-components/common-utilities/redirect-button/redirection-button";
-import Reviewthumbgalleryslidercomponents from "@packages/shared-components/common-utilities/slider/reviewthumbgalleryslidercomponents";
+import Reviewthumbgalleryslidercomponents from "@packages/shared-components/common-utilities/slider/reviewthumbgalleryslidercomponents"
+import Keystatscomponents from "@packages/shared-components/institution-profile/keystatscomponents"
 
 const page = () => {
   const skiplinkLabel = [
@@ -109,6 +110,8 @@ const page = () => {
                 bgColor={"bg-orange-200"}
               />
               <Ctabanner />
+              <Reviewthumbgalleryslidercomponents />
+              <Keystatscomponents />
               {/* <Reviewthumbgalleryslidercomponents /> */}
               <Findoutmore />
               <Pullquote />
