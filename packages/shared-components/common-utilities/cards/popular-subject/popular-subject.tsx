fd735e3ff00f07f8ba9c334b85413756d4220661
Ramkumar M <ramkumar.manokaran@idp.com>
2025-04-21 18:48:15 +0530

    HEW-3932 # conflict resolve

---
+++ packages/shared-components/common-utilities/cards/popular-subject/popular-subject.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const PopularSubject = ({
+  subjectTitle,
+  subjectBgColor,
+  leafBgColor,
+  rightArrowColor,
+}: any) => {
+  return (
+    <>
+      <div
+        className={`${subjectBgColor} shrink-0 rounded-[8px] w-[167px] h-full p-[12px]`}
+      >
+        <div className="flex flex-col gap-[32px] justify-between h-full">
+          <div className="small font-semibold text-grey900">{subjectTitle}</div>
+          <div
+            className={`${leafBgColor} size-[48px] rounded-[0_24px_0_24px] flex items-center justify-center self-end`}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                className={`${rightArrowColor}`}
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8.23798 2.55048C8.55528 2.23317 9.06972 2.23317 9.38702 2.55048L14.262 7.42548C14.5793 7.74278 14.5793 8.25722 14.262 8.57452L9.38702 13.4495C9.06972 13.7668 8.55528 13.7668 8.23798 13.4495C7.92067 13.1322 7.92067 12.6178 8.23798 12.3005L11.726 8.8125L2.3125 8.8125C1.86377 8.8125 1.5 8.44873 1.5 8C1.5 7.55127 1.86377 7.1875 2.3125 7.1875H11.726L8.23798 3.69952C7.92067 3.38222 7.92067 2.86778 8.23798 2.55048Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PopularSubject;
--- packages/shared-components/common-utilities/popular-subject-slider/popular-subject-slider.tsx
+++ packages/shared-components/common-utilities/popular-subject-slider/popular-subject-slider.tsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React from "react";
+import PopularSubject from "../cards/popular-subject/popular-subject";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { FreeMode, Pagination, Navigation } from "swiper/modules";
 
 const PopularSubjectSlider = () => {
   return (
-    <div>PopularSubjectSlider</div>
-  )
-}
+    <>
+      <div className="lg:w-[650px] xl:w-[907px] ">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={8}
+          freeMode={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+          }}
+          modules={[FreeMode, Pagination, Navigation]}
+          className="MultiSwiper popular"
+        >
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Architectural Engineering and Technology"}
+              subjectBgColor={"bg-blue-100"}
+              leafBgColor={"bg-blue-200"}
+              rightArrowColor={"fill-primary-500"}
+            />
+          </SwiperSlide>
 
-export default PopularSubjectSlider
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Wildlife and Conservation Biology"}
+              subjectBgColor={"bg-green-100"}
+              leafBgColor={"bg-green-200"}
+              rightArrowColor={"fill-secondary-500"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Creative Arts and Design"}
+              subjectBgColor={"bg-orange-100"}
+              leafBgColor={"bg-orange-200"}
+              rightArrowColor={"fill-orange-600"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Aerospace Engineering"}
+              subjectBgColor={"bg-grey-200"}
+              leafBgColor={"bg-grey-300"}
+              rightArrowColor={"fill-grey-600"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Forensic Nursing"}
+              subjectBgColor={"bg-blue-100"}
+              leafBgColor={"bg-blue-200"}
+              rightArrowColor={"fill-primary-500"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Wildlife and Conservation Biology"}
+              subjectBgColor={"bg-green-100"}
+              leafBgColor={"bg-green-200"}
+              rightArrowColor={"fill-secondary-500"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Creative Arts and Design"}
+              subjectBgColor={"bg-orange-100"}
+              leafBgColor={"bg-orange-200"}
+              rightArrowColor={"fill-orange-600"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Aerospace Engineering"}
+              subjectBgColor={"bg-grey-200"}
+              leafBgColor={"bg-grey-300"}
+              rightArrowColor={"fill-grey-600"}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PopularSubject
+              subjectTitle={"Forensic Nursing"}
+              subjectBgColor={"bg-blue-100"}
+              leafBgColor={"bg-blue-200"}
+              rightArrowColor={"fill-primary-500"}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default PopularSubjectSlider;
