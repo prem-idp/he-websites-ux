37f569704becb15679fdf1bc42d6757f89fe66fd
Ramkumar M <ramkumar.manokaran@idp.com>
2025-04-24 19:35:14 +0530

    HEW-3932 # commit fixes updates

--- packages/shared-components/common-utilities/advertiser/advertiser.tsx
+++ packages/shared-components/common-utilities/advertiser/advertiser.tsx
@@ -3,7 +3,12 @@ import WuscaBadge from "@packages/shared-components/common-utilities/wusca-badge
 import StudentRating from "@packages/shared-components/common-utilities/student-rating/student-rating";
 import StudentReviews from "@packages/shared-components/common-utilities/student-reviews/student-reviews";
 
-const Advertiser = ({ advertiserTitle, advertiserDescription, showWuscaCard = false }: any) => {
+const Advertiser = ({
+  advertiserTitle,
+  advertiserDescription,
+  showWuscaCard = false,
+  showreviewCard,
+}: any) => {
   return (
     <>
       {/* reviews */}
@@ -11,13 +16,13 @@ const Advertiser = ({ advertiserTitle, advertiserDescription, showWuscaCard = fa
         <div className="flex flex-col gap-[8px]">
           <div className="text-heading5 font-farro font-bold">
             {advertiserTitle}
-          </div>
-          <p> {advertiserDescription} </p>
+          </div>{" "}
+          {advertiserDescription && <p> {advertiserDescription} </p>}
         </div>
         <div className="border border-grey-200 bg-grey-50 rounded-[8px] overflow-hidden">
-          <WuscaBadge wuscaCard={showWuscaCard}/>
+          <WuscaBadge wuscaCard={showWuscaCard} />
           <StudentRating />
-          <StudentReviews />
+          {showreviewCard && <StudentReviews />}
         </div>
       </div>
       {/* reviews */}
