d820c5b53efa4bc7262ebdc61d951eade3660ff4
jessijanet <janet.raj@idp.com>
2025-04-29 11:47:30 +0530

    HEW-3932 # Space removed

--- apps/whatuni/src/app/why-study-here/page.tsx
+++ apps/whatuni/src/app/why-study-here/page.tsx
@@ -59,7 +59,7 @@ const page = () => {
       <section>
         <div className="max-w-container mx-auto px-[16px] md:px-[20px] xl:px-[0] py-[40px]">
           <div className="flex flex-col lg:flex-row gap-[20px]">
-            <div className="min-w-[289px] flex flex-col  relative max-w-[100%]">
+            <div className="min-w-[289px] flex flex-col relative max-w-[100%]">
               <Redirectlinkcomponent
                 data={skiplinkLabel}
                 activeLink={"Why study here?"}
