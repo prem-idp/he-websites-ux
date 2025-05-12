204b69017adcb247a67c47429301d42e759b8a04
Ramkumar M <ramkumar.manokaran@idp.com>
2025-05-12 15:01:14 +0530

    HEW-3932 #componentfixes card

--- packages/shared-components/institution-profile/opendaysvirtualadvertcomponents.tsx
+++ packages/shared-components/institution-profile/opendaysvirtualadvertcomponents.tsx
@@ -15,7 +15,11 @@ const Opendaysvirtualadvertcomponents = ({
           <div className="advert-card-inner">
             <div className="flex flex-col lg:flex-row gap-[16px]">
               {advertData?.map((item: any, index: number) => (
-                <Advertbannercard {...item} keyIndex={index} width={width} />
+                <Advertbannercard
+                  {...item}
+                  keyIndex={index}
+                  dataLength={advertData.length}
+                />
               ))}
             </div>
           </div>
