407378ac495ca0cdc30228494e47daa11405a2b4
Janet Cynthia <janet.raj@idp.com>
2025-06-06 17:13:07 +0530

    HEW-4570 #Feddback changes done

--- apps/whatuni/src/app/findacourse/page.tsx
+++ apps/whatuni/src/app/findacourse/page.tsx
@@ -7,17 +7,19 @@ import DegreeSubject from '@packages/shared-components/common-utilities/degree-s
 import Articlesnippetcomponents from '@packages/shared-components/common-utilities/article-snippet/articlesnippetcomponents';
 import Subscribecomponents from '@packages/shared-components/article-landing/subscribe-newsletter/subscribecomponents'
 import Advicecomponents from '@packages/shared-components/home/advice/advicecomponents'
+import DegreeSubjectSkeleton from '@packages/shared-components/common-utilities/skeleton/degree-subject-skeleton';
 
 const page = () => {
   const reviewText = "This is a really long review about the product. It has many details and descriptions, discussing various features, pros, and cons. Users should find this review very informative and helpful in making their purchase decision. We want to make sure that even long reviews can be displayed nicely without overflowing the layout, allowing users to expand them if they need more details. This sentence just makes it longer.";
 
   return (
     <>
-      <Findacoursecomponents {...findacourseData} bgColor="primary-100" removebtnDropdown = {false} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent"/>
-      <TabSwitchButton tabSwitchButtonData={tabDataDegree} />
+      <Findacoursecomponents {...findacourseData} bgColor="primary-100" removebtnDropdown={false} description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent" />
+      <TabSwitchButton tabSwitchButtonData={tabDataDegree}/>
       <Articlesnippetcomponents />
       <PopularDegree />
       <DegreeSubject />
+      {/* <DegreeSubjectSkeleton /> */}
       <Advicecomponents />
       <Subscribecomponents />
     </>
