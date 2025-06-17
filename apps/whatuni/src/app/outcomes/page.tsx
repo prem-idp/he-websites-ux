08577d1ef13b86ea6208966536facb3f38312a79
Janet Cynthia <janet.raj@idp.com>
2025-06-17 14:18:50 +0530

    HEW-5335 #Vepple date picker

--- apps/whatuni/src/app/outcomes/page.tsx
+++ apps/whatuni/src/app/outcomes/page.tsx
@@ -67,23 +67,19 @@ const page = () => {
   return (
     <>
       {/* clearing */}
-      {/* <div className="bg-green200 py-[12px] flex item-center justify-center gap-[14px]">
-        <div className="x-small">Show Clearing 2024 courses</div>
-        <div className="flex items-center">
-          <button
-            onClick={toggle}
-            className={`relative inline-flex items-center w-[42px] h-[24px] rounded-full transition-colors focus:outline-none ${
-              enabled ? "bg-secondary-500" : "bg-gray-300"
+      <section className="bg-green200 py-[12px] flex items-center justify-center gap-[14px]">
+        <div className="small">Show Clearing 2024 courses</div>
+        <button
+          onClick={toggle}
+          className={`relative inline-flex items-center w-[42px] h-[24px] rounded-full transition-colors focus:outline-none ${enabled ? "bg-secondary-500" : "bg-gray-400"
             }`}
-          > 
-            <span
-              className={`translate-x-0 transform transition ease-in-out duration-200 absolute inset-y-0 left-[2px] top-[2px] flex items-center justify-center h-[20px] w-[20px] rounded-full bg-white shadow-custom-13 ${
-                enabled ? "translate-x-[18px]" : "translate-x-0"
+        >
+          <span
+            className={`translate-x-0 transform transition ease-in-out duration-200 absolute inset-y-0 left-[2px] top-[2px] flex items-center justify-center h-[20px] w-[20px] rounded-full bg-white shadow-custom-13 ${enabled ? "translate-x-[18px]" : "translate-x-0"
               }`}
-            />
-          </button>
-        </div>
-      </div> */}
+          />
+        </button>
+      </section>
       {/* clearing */}
       <Interested />
       <HeaderBanner />
