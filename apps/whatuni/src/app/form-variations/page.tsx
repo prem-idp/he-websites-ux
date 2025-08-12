2a006f8865fbac569bf609f76d1cdf9a687ed7ee
Ramkumar M <117254689+RamkumarManokaran@users.noreply.github.com>
2025-08-12 17:47:53 +0530

    HEW-6096 #checkbox updated

--- apps/whatuni/src/app/form-variations/page.tsx
+++ apps/whatuni/src/app/form-variations/page.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import RadioComponent from '@packages/shared-components/common-utilities/form-variations/RadioComponent';
-import CheckboxComponent from '@packages/shared-components/common-utilities/form-variations/CheckboxComponent';
-import ToggleComponent from '@packages/shared-components/common-utilities/form-variations/ToggleComponent';
-import InputComponent from '@packages/shared-components/common-utilities/form-variations/InputComponent';
+import React from "react";
+import RadioComponent from "@packages/shared-components/common-utilities/form-variations/RadioComponent";
+import CheckboxComponent from "@packages/shared-components/common-utilities/form-variations/CheckboxComponent";
+import ToggleComponent from "@packages/shared-components/common-utilities/form-variations/ToggleComponent";
+import InputComponent from "@packages/shared-components/common-utilities/form-variations/InputComponent";
 
 const page = () => {
   return (
-    <>
-      <div className='max-w-container mx-auto py-10'>
-        <RadioComponent />
-        <CheckboxComponent />
-        <ToggleComponent />
-        <InputComponent />
-              </div>
-      </>
-  )
-}
+    <div className="max-w-container mx-auto py-10">
+      <RadioComponent />
+      <CheckboxComponent
+        title={"Control button heading"}
+        description={"Description of what this control will do"}
+        id={"checkbox-1"}
+      />
+      <ToggleComponent />
+      <InputComponent />
+    </div>
+  );
+};
 
-export default page
-
+export default page;
