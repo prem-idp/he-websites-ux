e917444ac1311c1ff614b6887b8ca8b99376c90b
Ramkumar M <117254689+RamkumarManokaran@users.noreply.github.com>
2025-08-14 20:07:14 +0530

    HEW-6149 #settings updates

---
+++ packages/shared-components/common-utilities/account-data/account-data.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export interface AccountDataProps {
+  data: {
+    title: string;
+    description: string | React.ReactNode;
+    buttonText: string;
+    popupBlocker?: string;
+    popupFunc?: boolean;
+  }[];
+}
+const AccountData = ({ data }: AccountDataProps) => {
+  // Assuming you want to render the first item in the data array
+  const { title, description, buttonText, popupBlocker } = data[0] || {};
+
+  return (
+    <div className="flex flex-col items-start gap-[16px]">
+      <div className="h6">{title}</div>
+      <div className="para">{description}</div>
+      <button className="btn btn-primary">{buttonText}</button>
+      {popupBlocker && <span className="pb-[8px]">{popupBlocker}</span>}
+    </div>
+  );
+};
+
+export default AccountData;
---
+++ packages/shared-components/common-utilities/settings/settings.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Link from "next/link";
+import AccountData from "@packages/shared-components/common-utilities/account-data/account-data";
+
+const Settings = () => {
+  const downloadData = [
+    {
+      title: "Download my data",
+      description: (
+        <>
+          When you register and use our services, we collect certain information
+          from you, like your full name and email address. Check out our{" "}
+          <Link className="text-primary-400 cursor-pointer pr-[4px]" href="#">
+            Privacy Notice
+          </Link>
+          for more details about the information that we collect and retain. You
+          can download a copy from here to access your data and this will only
+          be accessible to you.
+        </>
+      ),
+      buttonText: "Download my data",
+      popupBlocker:
+        "*Please make sure you have your popup blocker disabled before trying to download the PDF.",
+    },
+  ];
+  const deleteAccount = [
+    {
+      title: "Delete my account",
+      description: (
+        <>
+          We'll be sad to see you go, but if you want to delete your Whatuni,
+          Whatuni App, Postgraduate Search and IDP accounts, please click the
+          button below.
+        </>
+      ),
+      buttonText: "Delete my account",
+      popupFunc: true,
+    },
+  ];
+  return (
+    <div className="max-w-container my-[40px] mx-auto px-[16px] md:px-[20px] xl:px-0">
+      <div className="max-w-[800px] mx-auto flex flex-col gap-[32px]">
+        <div className="h4">Settings</div>
+        <span className="border-t border-grey-300"></span>
+        <AccountData data={downloadData} />
+        <span className="border-t border-grey-300"></span>
+        <AccountData data={deleteAccount} />
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
