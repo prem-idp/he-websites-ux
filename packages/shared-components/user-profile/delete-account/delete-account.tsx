20c1061124757e35b55b6a5857e69c0cb2a68690
Ramkumar M <117254689+RamkumarManokaran@users.noreply.github.com>
2025-08-19 15:08:47 +0530

    HEW-6149 #user-profile popup

--- packages/shared-components/user-profile/delete-account/delete-account.tsx
+++ packages/shared-components/user-profile/delete-account/delete-account.tsx
@@ -1,125 +1,220 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import CheckboxComponent from "@packages/shared-components/common-utilities/form-variations/CheckboxComponent";
+import Image from "next/image";
 
 const DeleteAccount = () => {
+  const [confirmpopup, setConfirmPopup] = useState(false);
   return (
     <div className="fixed top-0 left-0 w-full h-full backdrop-shadow flex items-center justify-center z-50">
-      <div className="bg-white shadow-custom-6 w-full md:w-[512px] p-[24px] rounded-[8px] mx-[16px]">
-        <div className="flex items-center justify-end mt-[-8px] mr-[-8px]">
-          <svg
-            width="10"
-            height="10"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              className="stroke-grey-400"
-              d="M1 13L13 1M1 1L13 13"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
-        <div className="review-modal-container flex flex-col gap-[24px]">
-          <div className="flex flex-col gap-[8px]">
-            <div className="h4">We're sorry to see you go!</div>
-            <p>
-              Please read the following important information before you
-              proceed.
-            </p>
-          </div>
-          {/* / */}
-          <div className="">
-            <span>Attention</span>
-            <div>
-              <p>
-                If you confirm your request, your data from IDP, Whatuni and
-                Postgraduate Search will be removed from our central systems and
-                your accounts will be deactivated. This will include deleting
-                your account from the Whatuni App.
-              </p>
-              <p>
-                If you delete your account and request prospectus [and a list of
-                actions, which will lead to a new registration] within 48 hours,
-                a new account will be created.
-              </p>
-            </div>
-          </div>
-          {/* Logo */}
-          <div></div>
-          {/* divider  */}
-          <span className="border-t border-grey-300"></span>
-          {/* END divider  */}
+      <div className="relative bg-white shadow-custom-6 w-full md:w-[575px] py-[40px] pl-[40px] pr-[25px] rounded-[8px] mx-[16px]">
+        <span className="absolute top-[16px] right-[16px] cursor-pointer">
+          <Image
+            alt="close icon"
+            width={20}
+            height={20}
+            src="/static/assets/icons/close_icon.svg"
+          />
+        </span>
+        <div className="overflow-y-auto max-h-[calc(100vh_-_150px)] md:max-h-[calc(100vh_-_100px)]">
+          {/* set 1 */}
+          {!confirmpopup && (
+            <div className="review-modal-container flex flex-col gap-[24px]">
+              <div className="flex flex-col gap-[8px]">
+                <div className="h4">We're sorry to see you go!</div>
+                <p>
+                  Please read the following important information before you
+                  proceed.
+                </p>
+              </div>
+              {/* / */}
+              <div className="flex items-start border border-orange-400 bg-orange-50 p-[16px] rounded-[8px] gap-[8px]">
+                <Image
+                  width={20}
+                  height={20}
+                  alt="attention icon"
+                  src="/static/assets/icons/user-profile/alert-icon-orange.svg"
+                />
 
-          <div>
-            <div className="h5">Are you sure you wish to proceed?</div>
-            <p>
-              Instead of leaving us, would you like to opt out of our marketing
-              communications?
-            </p>
-            <span>
-              If you wish to opt out of marketing related to this app, please
-              unsubscribe
-            </span>
-            <button>Unsubscribe</button>
-          </div>
-          {/* divider  */}
-          <span className="border-t border-grey-300"></span>
-          {/* END divider  */}
+                <div className="flex flex-col gap-[4px] text-small text-tertiary-600">
+                  <span className="font-semibold">Attention</span>
+                  <div className="flex flex-col gap-[16px]">
+                    <p>
+                      If you confirm your request, your data from IDP, Whatuni
+                      and Postgraduate Search will be removed from our central
+                      systems and your accounts will be deactivated. This will
+                      include deleting your account from the Whatuni App.
+                    </p>
+                    <p>
+                      If you delete your account and request prospectus [and a
+                      list of actions, which will lead to a new registration]
+                      within 48 hours, a new account will be created.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* Logo */}
+              <div className="w-full items-center flex justify-start gap-[10px]">
+                <button>
+                  <Image
+                    width={90}
+                    height={50}
+                    alt="idp logo"
+                    src="/static/assets/icons/IDP_Logo.svg"
+                  />
+                </button>
+                <button>
+                  <Image
+                    width={45}
+                    height={48}
+                    alt="Whatuni Logo"
+                    src="/static/assets/images/whatuni-logo.svg"
+                  />
+                </button>
+                <button>
+                  <Image
+                    width={130}
+                    height={34}
+                    alt="POSTGRADUATE_SEARCH"
+                    src="https://images.ctfassets.net/szez98lehkfm/6Z2XBvZNThCE23P5umA60L/69f5f95d023ed4bea44d349251e1991b/POSTGRADUATE_SEARCH_RGB.svg"
+                  />
+                </button>
+              </div>
+              {/* divider  */}
+              <span className="border-t border-grey-300"></span>
+              {/* END divider  */}
 
-          <div>
-            <div className="h5">Reasons behind deleting your accounts</div>
-            <p>
-              Let us know why you want to delete your account so we can know if
-              there's anything better we can do in the future (optional)
-            </p>
-            <div className="">
-              <label htmlFor=""></label>
-              <select name="" id="">
-                <optin>Please choose</optin>
-              </select>
-            </div>
-          </div>
+              <div className="flex items-start flex-col gap-[8px]">
+                <div className="h5">Are you sure you wish to proceed?</div>
+                <p>
+                  Instead of leaving us, would you like to opt out of our
+                  marketing communications?
+                </p>
+                <span className="text-grey-400">
+                  If you wish to opt out of marketing related to this app,
+                  please unsubscribe
+                </span>
+                <button className="flex font-semibold para items-center gap-[10px] text-primary-400">
+                  Unsubscribe
+                  <Image
+                    height={10}
+                    alt="right arrow"
+                    width={14}
+                    src="/static/assets/icons/arrow-right.svg"
+                  />
+                </button>
+              </div>
+              {/* divider  */}
+              <span className="border-t border-grey-300"></span>
+              {/* END divider  */}
 
-          {/* divider  */}
-          <span className="border-t border-grey-300"></span>
-          {/* END divider  */}
-          <div>
-            <div className="h5">
-              Please review our privacy notice regarding the right to erasure or
-              personal information
+              <div className="flex items-start flex-col gap-[8px]">
+                <div className="h5">Reasons behind deleting your accounts</div>
+                <p>
+                  Let us know why you want to delete your account so we can know
+                  if there's anything better we can do in the future (optional)
+                </p>
+                <div className="w-full flex flex-col text-grey300 gap-[4px]">
+                  <label
+                    className="font-semibold text-grey300"
+                    htmlFor="reason-leaving"
+                  >
+                    Reason for leaving
+                  </label>
+                  <select
+                    className="w-full rounded-[4px] cursor-pointer border border-grey-500 bg-grey-100 py-[10px] px-[12px] rounded-[4px] text-small text-grey-600"
+                    name="Reason for leaving"
+                    id="reason-leaving"
+                  >
+                    <option selected>Please choose</option>
+                    <option>Review our privacy 1</option>
+                    <option>Review our privacy 2</option>
+                    <option>Review our privacy 3</option>
+                    <option>Review our privacy 4</option>
+                    <option>Review our privacy 5</option>
+                  </select>
+                </div>
+              </div>
+
+              {/* divider  */}
+              <span className="border-t border-grey-300"></span>
+              {/* END divider  */}
+              <div className="flex items-start flex-col gap-[8px]">
+                <div className="h6">
+                  Please review our privacy notice regarding the right to
+                  erasure or personal information
+                </div>
+                <div className="">
+                  <CheckboxComponent
+                    title={""}
+                    description={
+                      <>
+                        I have read and understood the {""}
+                        <Link
+                          className="text-primary-400 cursor-pointer pr-[4px]"
+                          href="#"
+                        >
+                          Privacy Notice
+                        </Link>
+                        relating to the deletion of personal infomation
+                      </>
+                    }
+                    id={"checkbox-1"}
+                  />
+                  <CheckboxComponent
+                    title={""}
+                    description={
+                      "I understand that my details associated with this email address will be removed from IDP, Whatuni, Whatuni App and Postgraduate Search"
+                    }
+                    id={"checkbox-2"}
+                  />
+                </div>
+              </div>
+              {/*  */}
+              <button
+                onClick={() => setConfirmPopup(true)}
+                className="btn btn-primary"
+              >
+                Delete my account
+              </button>
             </div>
-            <div className="">
-              <CheckboxComponent
-                title={""}
-                description={
-                  <>
-                    I have read and understood the
-                    <Link
-                      className="text-primary-400 cursor-pointer pr-[4px]"
-                      href="#"
-                    >
-                      Privacy Notice
-                    </Link>
-                    relating to the deletion of personal infomation
-                  </>
-                }
-                id={"checkbox-1"}
-              />
-              <CheckboxComponent
-                title={""}
-                description={
-                  "I understand that my details associated with this email address will be removed from IDP, Whatuni, Whatuni App and Postgraduate Search"
-                }
-                id={"checkbox-2"}
-              />
+          )}
+
+          {/* set 2 */}
+          {confirmpopup && (
+            <div className="review-modal-container flex flex-col">
+              <div className="w-full mb-[16px] flex gap-[8px] bg-green-100 border border-positive-dark rounded-[6px] p-[16px]">
+                <Image
+                  alt="tick icon"
+                  width={20}
+                  height={20}
+                  src="/static/assets/icons/green_tick_icon.svg"
+                />
+                <div className="flex flex-grow small font-semibold text-positive-dark">
+                  Your data will be erased as soon as possible
+                </div>
+              </div>
+              <div className="flex items-start flex-col gap-[8px]">
+                <div className="h4">Your next steps</div>
+                <span className="font-semibold para-lg text-positive-default">
+                  Your data will be erased within 30 days
+                </span>
+                <p>
+                  You will receive an email to confirm that your data has been
+                  erased within 30 days. If you change your mind after your data
+                  has been deleted, you will be able to create a new account.
+                </p>
+              </div>
+              {/* divider  */}
+              <span className="border-t border-grey-300 my-[24px]"></span>
+              <button
+                onClick={() => setConfirmPopup(false)}
+                className="btn btn-primary"
+              >
+                Close
+              </button>
             </div>
-          </div>
-          {/*  */}
-          <button className="btn btn-primary">Delete my account</button>
+          )}
         </div>
       </div>
     </div>
