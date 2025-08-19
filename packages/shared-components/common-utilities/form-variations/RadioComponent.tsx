463485df6fcfd608d696b7f64f48bf48da66ed98
prem-idp <premkumar.mannu@idp.com>
2025-08-19 12:11:47 +0530

    HEW-6150 #Profile form changes moved

--- packages/shared-components/common-utilities/form-variations/RadioComponent.tsx
+++ packages/shared-components/common-utilities/form-variations/RadioComponent.tsx
@@ -8,8 +8,14 @@ type RadioProps = {
 
 const RadioComponent = ({id, label, description }: RadioProps) => {
   return (
-    <div>RadioComponent</div>
-  )
-}
+      <div className="flex items-center gap-[12px]">
+        <input type="radio" name="control" id={`radio ${id}`} className="self-start size-[16px] border border-grey-400 accent-primary-400 mt-[4px]"/>
+        <label htmlFor={`radio ${id}`}>
+          {label?.trim() && <p className="x-small font-semibold text-grey-600">{label}</p>}
+          {description?.trim() && <p className="small font-normal text-grey-600">{description}</p>}
+        </label>
+      </div>
+  );
+};
 
 export default RadioComponent;
