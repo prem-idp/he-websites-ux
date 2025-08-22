6c3a8b49b79b83b20db753697dfd01efa11f1917
prem-idp <premkumar.mannu@idp.com>
2025-08-22 16:12:18 +0530

    HEW-6150 #Merge branch 'UX-PREM' into he-websites-ux

--- apps/whatuni/src/app/user-profile/activity/layout.tsx
+++ apps/whatuni/src/app/user-profile/activity/layout.tsx
@@ -1,12 +1,29 @@
+"use client";
 import { ReactNode } from "react";
+import ProfileUpdates from "@packages/shared-components/common-utilities/profile-updates/profile-updates";
+import SettingTabs from "@packages/shared-components/common-utilities/setting-tabs/setting-tabs";
 import Activity from "@packages/shared-components/common-utilities/activity/activity";
 import Subscribecomponents from "@packages/shared-components/article-landing/subscribe-newsletter/subscribecomponents";
 
 export default function ActivityLayout({ children }: { children: ReactNode }) {
+  const listData = [
+    { title: "Favourites", id: 1 },
+    { title: "Profile", id: 2 },
+    { title: "Activity", id: 3 },
+    { title: "Settings", id: 4 },
+  ];
+
   return (
-    <>
+    <div>
+      <ProfileUpdates>
+        <SettingTabs
+          data={listData}
+          selectedTab="Activity"
+          onSelectTab={() => {}}
+        />
+      </ProfileUpdates>
       <Activity>{children}</Activity>
       <Subscribecomponents />
-    </>
+    </div>
   );
 }
