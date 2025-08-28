c8bf6708d8897d34a621cb134c12615ad1141bf6
Janet Cynthia <janet.raj@idp.com>
2025-08-28 13:39:35 +0530

    HEW-6151 #Build error

--- apps/whatuni/src/app/user-profile/favourites/page.tsx
+++ apps/whatuni/src/app/user-profile/favourites/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Favourites from "@packages/shared-components/user-profile/favourites/favourites";
 import ManageFavourites from "@packages/shared-components/user-profile/favourites/manage-favourites/managefavourites";
 import Advicecomponents from "@packages/shared-components/home/advice/advicecomponents";
-import CourseResult from "@packages/shared-components/user-profile/favourites/course-result/CourseResult";
+// import CourseResult from "@packages/shared-components/user-profile/favourites/course-result/CourseResult";
 import Subscribecomponents from "@packages/shared-components/article-landing/subscribe-newsletter/subscribecomponents";
 
 const FavouritesPage = () => {
@@ -10,17 +10,15 @@ const FavouritesPage = () => {
 
   return (
     <>
-      {/* <Favourites />
+      <Favourites />
       <ManageFavourites />
       <Advicecomponents
         bgColor={bgColor1}
         heading={"Open days advice and articles"}
         subheading={""}
-      /> */}
+      />
       <section className="bg-white p-[16px] md:px-[20px] lg:pt-[16px] xl:px-0">
-        <div className="max-w-container mx-auto">
-          <CourseResult />
-        </div>
+        <div className="max-w-container mx-auto">{/* <CourseResult /> */}</div>
       </section>
       <Subscribecomponents />
     </>
