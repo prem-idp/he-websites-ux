468e726478a3ea763d108a2c8992b3d852d19115
Janet Cynthia <janet.raj@idp.com>
2026-03-20 11:22:25 +0530

    DLG-8189 #forgot password and unsubscribe

--- packages/shared-components/common-utilities/register-new/forgotpassword-otp.tsx
+++ packages/shared-components/common-utilities/register-new/forgotpassword-otp.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+import {
+  HideEyeIcon,
+  ShowEyeIcon,
+} from "../../../../apps/whatuni/src/app/media-utilities/mediautilities";
 
 const ForgotpasswordOtp = () => {
   const [password, setPassword] = useState("");
@@ -44,7 +47,9 @@ const ForgotpasswordOtp = () => {
   const validateConfirmPassword = (val: string) => {
     setConfirmPassword(val);
     setConfirmPasswordError(
-      val && val !== password ? "Passwords do not match" : "",
+      val && val !== password
+        ? "Your passwords do not match, please try again"
+        : "",
     );
   };
   return (
@@ -117,7 +122,7 @@ const ForgotpasswordOtp = () => {
             type={showPassword ? "text" : "password"}
             value={password}
             onChange={(e) => validatePassword(e.target.value)}
-            className={`w-full small font-normal px-[12px] py-[10px] pr-[40px] border rounded-[4px] outline-none shadow-custom-2 ${passwordError ? "border-negative-default" : "border-grey-500"}`}
+            className={`w-full small px-[12px] py-[10px] pr-[40px] border rounded-[4px] outline-none shadow-custom-2 ${passwordError ? "border-negative-default" : "border-grey-500"}`}
             id="password"
             placeholder="Please enter your password"
           />
@@ -128,46 +133,7 @@ const ForgotpasswordOtp = () => {
             type="button"
             onClick={() => setShowPassword(!showPassword)}
           >
-            {showPassword ? (
-              <svg
-                width="16"
-                height="13"
-                viewBox="0 0 16 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10.2004 6.50032C10.2004 7.71576 9.21511 8.70107 7.99967 8.70107C6.78423 8.70107 5.79892 7.71576 5.79892 6.50032C5.79892 5.28488 6.78423 4.29957 7.99967 4.29957C9.21511 4.29957 10.2004 5.28488 10.2004 6.50032Z"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M1 6.5003C1.93479 3.52408 4.71528 1.36523 7.99999 1.36523C11.2847 1.36523 14.0652 3.52411 15 6.50035C14.0652 9.47656 11.2847 11.6354 8.00001 11.6354C4.71527 11.6354 1.93477 9.47654 1 6.5003Z"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            ) : (
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3.39742 3.39844L6.03037 6.03139M16.6019 16.6029L13.9692 13.9703M11.375 15.0071C10.9296 15.0916 10.47 15.1358 10 15.1358C6.71527 15.1358 3.93477 12.9769 3 10.0007C3.25448 9.19045 3.64574 8.4408 4.14672 7.7788M8.4435 8.44452C8.84176 8.04627 9.39195 7.79994 9.99967 7.79994C11.2151 7.79994 12.2004 8.78525 12.2004 10.0007C12.2004 10.6084 11.9541 11.1586 11.5558 11.5569M8.4435 8.44452L11.5558 11.5569M8.4435 8.44452L6.03037 6.03139M11.5558 11.5569L6.03037 6.03139M11.5558 11.5569L13.9692 13.9703M6.03037 6.03139C7.17463 5.29368 8.53732 4.86561 9.99999 4.86561C13.2847 4.86561 16.0652 7.02448 17 10.0007C16.4814 11.6518 15.3948 13.0513 13.9692 13.9703"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            )}
+            {showPassword ? <ShowEyeIcon /> : <HideEyeIcon />}
           </button>
         </div>
         {passwordError && (
@@ -194,7 +160,7 @@ const ForgotpasswordOtp = () => {
             type={showConfirmPassword ? "text" : "password"}
             value={confirmPassword}
             onChange={(e) => validateConfirmPassword(e.target.value)}
-            className={`w-full small font-normal px-[12px] py-[10px] pr-[40px] border rounded-[4px] outline-none shadow-custom-2 ${confirmPasswordError ? "border-negative-default" : "border-grey-500"}`}
+            className={`w-full small px-[12px] py-[10px] pr-[40px] border rounded-[4px] outline-none shadow-custom-2 ${confirmPasswordError ? "border-negative-default" : "border-grey-500"}`}
             id="confirmpassword"
             placeholder="Please enter your password"
           />
@@ -204,46 +170,7 @@ const ForgotpasswordOtp = () => {
             type="button"
             onClick={() => setShowConfirmPassword(!showConfirmPassword)}
           >
-            {showConfirmPassword ? (
-              <svg
-                width="16"
-                height="13"
-                viewBox="0 0 16 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10.2004 6.50032C10.2004 7.71576 9.21511 8.70107 7.99967 8.70107C6.78423 8.70107 5.79892 7.71576 5.79892 6.50032C5.79892 5.28488 6.78423 4.29957 7.99967 4.29957C9.21511 4.29957 10.2004 5.28488 10.2004 6.50032Z"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M1 6.5003C1.93479 3.52408 4.71528 1.36523 7.99999 1.36523C11.2847 1.36523 14.0652 3.52411 15 6.50035C14.0652 9.47656 11.2847 11.6354 8.00001 11.6354C4.71527 11.6354 1.93477 9.47654 1 6.5003Z"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            ) : (
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3.39742 3.39844L6.03037 6.03139M16.6019 16.6029L13.9692 13.9703M11.375 15.0071C10.9296 15.0916 10.47 15.1358 10 15.1358C6.71527 15.1358 3.93477 12.9769 3 10.0007C3.25448 9.19045 3.64574 8.4408 4.14672 7.7788M8.4435 8.44452C8.84176 8.04627 9.39195 7.79994 9.99967 7.79994C11.2151 7.79994 12.2004 8.78525 12.2004 10.0007C12.2004 10.6084 11.9541 11.1586 11.5558 11.5569M8.4435 8.44452L11.5558 11.5569M8.4435 8.44452L6.03037 6.03139M11.5558 11.5569L6.03037 6.03139M11.5558 11.5569L13.9692 13.9703M6.03037 6.03139C7.17463 5.29368 8.53732 4.86561 9.99999 4.86561C13.2847 4.86561 16.0652 7.02448 17 10.0007C16.4814 11.6518 15.3948 13.0513 13.9692 13.9703"
-                  stroke="#5C656E"
-                  strokeWidth="1.67"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            )}
+            {showConfirmPassword ? <ShowEyeIcon /> : <HideEyeIcon />}
           </button>
         </div>
         {confirmPasswordError && (
