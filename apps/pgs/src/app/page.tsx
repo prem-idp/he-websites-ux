64f7b0071f2639d3ddc8b9eb8ac287efaf2074af
Janet Cynthia <janet.raj@idp.com>
2026-05-14 17:00:00 +0530

    DLG-003 #PGS build error fixed

--- apps/pgs/src/app/page.tsx
+++ apps/pgs/src/app/page.tsx
@@ -7,12 +7,71 @@ import ReviewComponent from "@packages/shared-components/home/reviews/reviewscom
 import TestimonialComponent from "@packages/shared-components/home/testimonials/testimonialcomponents";
 import Wuscascomponents from "@packages/shared-components/home/wuscas/wuscascomponents";
 import OurPartnerComponent from "@packages/shared-components/common-utilities/our-partners/ourpartnercomponent";
+import { DiscoverCardData } from "@packages/shared-components/common-utilities/slider/discovercard";
+
+const discoverCards: DiscoverCardData[] = [
+  {
+    label: "courses",
+    title: "Looking for courses?",
+    href: "",
+    bgColor: "bg-blue-100",
+    tagColor: "text-primary-500",
+    tagBg: "bg-white/[.6]",
+    image: "/static/assets/images/discover/discover-feature-image1x3x.png",
+  },
+  {
+    label: "universities",
+    title: "Pick your perfect uni",
+    href: "",
+    bgColor: "bg-secondary-200",
+    tagColor: "text-positive-dark",
+    tagBg: "bg-white/[.6]",
+    image: "/static/assets/images/discover/discover-feature-image2x3x.png",
+  },
+  {
+    label: "career",
+    title: "Take our careers quiz",
+    href: "",
+    bgColor: "bg-tertiary-100",
+    tagColor: "text-negative-dark",
+    tagBg: "bg-white/[.6]",
+    image: "/static/assets/images/discover/discover-feature-image3x3x.png",
+  },
+  {
+    label: "Subject guides",
+    title: "Find out what to study",
+    href: "",
+    bgColor: "bg-blue-100",
+    tagColor: "text-primary-500",
+    tagBg: "bg-white/[.6]",
+    image: "/static/assets/images/discover/discover-feature-image1x3x.png",
+  },
+  {
+    label: "open days",
+    title: "Find an open day",
+    href: "",
+    bgColor: "bg-secondary-200",
+    tagColor: "text-positive-dark",
+    tagBg: "bg-positive-light",
+    image: "/static/assets/images/discover/discover-feature-image2x3x.png",
+  },
+  {
+    label: "app",
+    title: "Download the app",
+    href: "",
+    bgColor: "bg-tertiary-100",
+    tagColor: "text-negative-dark",
+    tagBg: "bg-white/[.6]",
+    image: "/static/assets/images/discover/discover-feature-image3x3x.png",
+  },
+];
+
 const page = () => {
   return (
     <>
       <HeroSliderComponent project="pgs" />
       <Wuscascomponents />
-      <Discovercomponents />
+      <Discovercomponents cards={discoverCards} />
       <AdviceComponent />
       <TestimonialComponent />
       <ReviewComponent />
